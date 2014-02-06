e92d5558591d85142584827d25612664b514e15f
Chris Beer <cabeer@stanford.edu>
2014-02-05 22:42:55 -0800

    use STI for storing different types of pages

---
+++ app/models/spotlight/page.rb
@@ -0,0 +1,31 @@
+module Spotlight
+  class Page < ActiveRecord::Base
+    MAX_PAGES = 50
+
+    belongs_to :exhibit
+    validates :weight, :inclusion => { :in => Proc.new{ 0..Spotlight::Page::MAX_PAGES } }
+    validates :exhibit, presence: true
+
+    default_scope { order("weight ASC") }
+    scope :at_top_level, -> { where(parent_page_id: nil) }
+    scope :published, -> { where(published: true) }
+    
+    # explicitly set the partial path so that 
+    # we don't have to duplicate view logic.
+    def to_partial_path
+      "spotlight/pages/page"
+    end
+
+    def feature_page?
+      is_a? FeaturePage
+    end
+
+    def about_page?
+      is_a? AboutPage
+    end
+
+    def top_level_page?
+      try(:parent_page).blank?
+    end
+  end
+end
