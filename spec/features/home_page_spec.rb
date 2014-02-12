08a84ef9dc7b8200513744cf74de38b05bfba3f1
Chris Beer <cabeer@stanford.edu>
2014-02-12 11:38:08 -0800

    navigate to curator pages using the global nav bar

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -3,7 +3,10 @@ describe "Home page" do
   let(:exhibit_curator) { FactoryGirl.create(:exhibit_curator) }
   before {login_as exhibit_curator}
   it "should exist by default on exhibits" do
-    visit spotlight.exhibit_catalog_index_path(Spotlight::Exhibit.default)
+    visit '/'
+    within '.dropdown-menu' do
+      click_link 'Curation'
+    end
     click_link "Feature pages"
     expect(page).to have_selector "h3", text: "Exhibit Home"
   end
