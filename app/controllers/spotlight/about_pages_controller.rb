30edb43577c41429042b1e3b8978757329e6d254
Justin Coyne <justin@curationexperts.com>
2014-02-13 16:04:26 -0600

    Refactor PagesController#page_model

--- app/controllers/spotlight/about_pages_controller.rb
+++ app/controllers/spotlight/about_pages_controller.rb
@@ -2,9 +2,6 @@ module Spotlight
   class AboutPagesController < PagesController
 
     private
-    def page_model
-      :about_page
-    end
     def cast_page_instance_variable
       if @about_pages
         @pages = @about_pages
