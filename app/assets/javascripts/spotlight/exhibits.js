051a7b3965e91bbac514705b3cd01a0b4da2fae5
Jessie Keck <jessie.keck@gmail.com>
2014-02-13 12:38:21 -0800

    Don't duplicate contact field IDs when cloning the input.
    Also adding a javascript integration test for the Add Contact button.

--- app/assets/javascripts/spotlight/exhibits.js
+++ app/assets/javascripts/spotlight/exhibits.js
@@ -3,6 +3,7 @@ Spotlight.onLoad(function() {
     container = $(this).parent().parent();
     input = container.find('.input-group input[type="text"]').first().clone();
     input.val('');
+    input.attr('id', input.attr('id').replace('0', container.children().length));
     input.attr('name', input.attr('name').replace('0', container.children().length));
     container.append(input);
   });
