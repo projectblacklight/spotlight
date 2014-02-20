1ae3d88126d45878b3b34e39377294ee440868d9
Jessie Keck <jessie.keck@gmail.com>
2014-02-19 21:52:04 -0800

    Add "private" label to non-public items
    The Public checkbox is submitted via ajax so this adds a function as
    a 'success' callback to toggle the label in the thumbnail-image div.
    
    Fixes #246

--- app/assets/javascripts/spotlight/catalog_edit.js
+++ app/assets/javascripts/spotlight/catalog_edit.js
@@ -31,7 +31,14 @@ Spotlight.onLoad(function() {
 
       $(".visiblity_toggle").bl_checkbox_submit({
           //css_class is added to elements added, plus used for id base
-          css_class: "toggle_visibility"    
+          css_class: "toggle_visibility",
+          //success is called at the end of the ajax success callback
+          success: function (){
+            // We store the selector of the label to toggle in a data attribute in the form
+            var private_label = $($(this).data("label-toggle-target"));
+            // Toggle the Private label
+            private_label.slideToggle();
+          }
       }); 
 
 });
