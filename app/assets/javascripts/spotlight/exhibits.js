5d38743addcbec203b1ea3f2c953ac51e1252d4b
Chris Beer <cabeer@stanford.edu>
2014-02-21 08:38:35 -0800

    rework contact 'another email' feature to play nice with a help block

--- app/assets/javascripts/spotlight/exhibits.js
+++ app/assets/javascripts/spotlight/exhibits.js
@@ -3,9 +3,9 @@ Spotlight.onLoad(function() {
     container = $(this).parent().parent();
     input = container.find('.input-group input[type="text"]').first().clone();
     input.val('');
-    input.attr('id', input.attr('id').replace('0', container.children().length));
-    input.attr('name', input.attr('name').replace('0', container.children().length));
-    container.append(input);
+    input.attr('id', input.attr('id').replace('0', container.find('input[type="text"]').length));
+    input.attr('name', input.attr('name').replace('0', container.find('input[type="text"]').length));
+    input.insertBefore(container.find('.help-block'));
   });
 
   $('.btn-with-tooltip').tooltip();
