1d423cc21e0243c61f0dc4149664e2ec69fa8609
Justin Coyne <justin@curationexperts.com>
2014-02-27 13:38:48 -0600

    Display the confirmation state of the contact_emails

--- spec/features/confirm_email_spec.rb
+++ spec/features/confirm_email_spec.rb
@@ -8,6 +8,10 @@ describe "Confirming an email" do
   it "should resend confirmation instructions" do
     visit spotlight.new_contact_email_confirmation_url(:confirmation_token => contact_email.confirmation_token)
     expect(page).to have_content("Resend confirmation instructions")
+    within '#new_contact_email' do
+      fill_in 'Email', with: contact_email.email
+      click_button "Resend confirmation instructions"
+    end
   end
 
   it "should confirm email" do
