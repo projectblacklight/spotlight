e6dc54fa60e0847bb9836170aaddc97e1e8a3129
Justin Coyne <justin@curationexperts.com>
2014-02-27 11:14:39 -0600

    ContactEmail should be confirmable

--- app/models/spotlight/contact_email.rb
+++ app/models/spotlight/contact_email.rb
@@ -1,13 +1,17 @@
 module Spotlight
   class ContactEmail < ActiveRecord::Base
+    extend Devise::Models
+    devise :confirmable
     belongs_to :exhibit
     validate :valid_email
+    validates :exhibit, presence: true
 
     def to_s
       email
     end
 
     protected 
+
     def valid_email
       begin
         parsed = Mail::Address.new(email)
@@ -16,5 +20,16 @@ module Spotlight
       errors.add :email, "is not valid" unless !parsed.nil? && parsed.address == email && parsed.local != email #cannot be a local address
     end
 
+    def send_devise_notification(notification, *args)
+      notification_mailer.send(notification, self, *args).deliver
+    end
+
+    def notification_mailer
+      Spotlight::ConfirmationMailer
+    end
+
   end
 end
+
+
+
