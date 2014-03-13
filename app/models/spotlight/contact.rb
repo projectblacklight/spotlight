a26a86e905b80ecabb526ea2d3d69000a2e45704
Chris Beer <cabeer@stanford.edu>
2014-03-13 09:42:03 -0700

    add friendly id to custom fields and contacts
    ref #322

--- app/models/spotlight/contact.rb
+++ app/models/spotlight/contact.rb
@@ -2,4 +2,13 @@ class Spotlight::Contact < ActiveRecord::Base
   belongs_to :exhibit
   scope :published, -> { where(show_in_sidebar: true) }
   default_scope { order("weight ASC") }
+  
+  extend FriendlyId
+  friendly_id :name, use: [:slugged,:scoped,:finders], scope: :exhibit
+
+  protected
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
+
 end
--- app/models/spotlight/custom_field.rb
+++ app/models/spotlight/custom_field.rb
@@ -2,6 +2,9 @@ module Spotlight
   class CustomField < ActiveRecord::Base
     serialize :configuration, Hash
     belongs_to :exhibit
+    
+    extend FriendlyId
+    friendly_id :slug_candidates, use: [:slugged,:scoped,:finders], scope: :exhibit
 
     before_save do
       self.field ||= field_name
@@ -37,6 +40,18 @@ module Spotlight
 
     def field_name
       CustomField.with_suffix("exhibit_#{self.exhibit.to_param}_#{label.parameterize}")
+    end
+
+    def should_generate_new_friendly_id?
+      true
+    end  
+    # Try building a slug based on the following fields in
+    # increasing order of specificity.
+    def slug_candidates
+      [
+        :label,
+        :field
+      ]
     end
 
   end
