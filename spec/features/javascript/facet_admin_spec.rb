58261f3201c0f2e2749fb4745f8b2df33b3bb42f
Jessie Keck <jessie.keck@gmail.com>
2014-03-18 16:48:37 -0700

    Make facet label edit-in-place.

---
+++ spec/features/javascript/facet_admin_spec.rb
@@ -0,0 +1,37 @@
+require "spec_helper"
+
+feature "Facets Administration", js:  true do
+  let(:exhibit) { FactoryGirl.create(:exhibit) }
+  let(:exhibit_curator) { FactoryGirl.create(:exhibit_curator, exhibit: exhibit) }
+  before { login_as exhibit_curator }
+  it "should allow us to update the label with edit-in-place" do
+    pending("Passing locally but Travis is throwing intermittent errors") if ENV["CI"]
+
+    input_id = "blacklight_configuration_facet_fields_genre_ssim_label"
+    visit spotlight.exhibit_home_page_path(exhibit, exhibit.home_page)
+    click_link exhibit_curator.email
+
+    within '#user-util-collapse .dropdown' do
+      click_link 'Dashboard'
+    end
+
+    click_link "Search facets"
+
+    facet = find(".edit-in-place", text: "Genre")
+    expect(page).not_to have_content("Topic")
+    expect(page).to have_css("input##{input_id}", visible: false)
+
+    facet.click
+
+    expect(page).to have_css("input##{input_id}", visible: true)
+
+    fill_in(input_id, with: "Topic")
+
+    click_button "Save changes"
+
+    expect(page).to have_content("The exhibit was successfully updated.")
+
+    expect(page).not_to have_content("Genre")
+    expect(page).to have_content("Topic")
+  end
+end
