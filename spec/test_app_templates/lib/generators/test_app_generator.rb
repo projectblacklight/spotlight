172927ff9e93cb36643c30daeb75e52444018918
Chris Beer <cabeer@stanford.edu>
2014-06-30 08:32:49 -0700

    Update spotlight:install generator to use configurable defaults for optional add-ons

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -18,7 +18,6 @@ class TestAppGenerator < Rails::Generators::Base
     say_status("warning", "GENERATING BL", :yellow)
 
     generate 'blacklight:install', '--devise'
-    copy_file "catalog_controller.rb", "app/controllers/catalog_controller.rb", force: true
   end
 
   def run_spotlight_migrations
@@ -27,15 +26,14 @@ class TestAppGenerator < Rails::Generators::Base
   end
 
   def add_spotlight_routes_and_assets
-    generate 'spotlight:install'
+    generate 'spotlight:install', '--mailer_default_url_host=localhost:3000'
+  end
+
+  def install_test_catalog_controller
+    copy_file "catalog_controller.rb", "app/controllers/catalog_controller.rb", force: true
   end
 
   def add_rake_tasks_to_app
     rakefile 'spotlight_test.rake', File.read(find_in_source_paths('spotlight_test.rake'))
-  end
-
-  def add_mailer_defaults
-    mail_config = "    config.action_mailer.default_url_options = { host: \"localhost:3000\", from: \"noreply@example.com\" }\n"
-    insert_into_file 'config/application.rb', mail_config, after: "< Rails::Application\n"
   end
 end
--- template.rb
+++ template.rb
@@ -7,9 +7,17 @@ gem "jettywrapper"
 
 run "bundle install"
 
-generate 'blacklight:install', '--devise'
-generate 'blacklight_gallery:install'
-generate 'spotlight:install'
+# run the blacklight install generator
+blacklight_options = ENV.fetch("BLACKLIGHT_INSTALL_OPTIONS", '--devise --jettywrapper')
+generate 'blacklight:install', blacklight_options
+
+blacklight_gallery_options = ENV.fetch("BLACKLIGHT_GALLERY_INSTALL_OPTIONS", '')
+if blacklight_gallery_options != false
+  generate 'blacklight_gallery:install', blacklight_gallery_options
+end
+
+spotlight_options = ENV.fetch("SPOTLIGHT_INSTALL_OPTIONS", '--openseadragon --mailer_default_url_host=localhost:3000')
+generate 'spotlight:install', spotlight_options
 rake "spotlight:install:migrations"
 rake "db:migrate"
 
