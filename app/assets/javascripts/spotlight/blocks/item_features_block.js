c1c262c37363700ba452ccb1daf56ab6a156e63b
Chris Beer <cabeer@stanford.edu>
2014-11-26 14:36:27 -0800

    Trigger known block javascript behaviors when previewing

--- app/assets/javascripts/spotlight/blocks/item_features_block.js
+++ app/assets/javascripts/spotlight/blocks/item_features_block.js
@@ -13,12 +13,21 @@ SirTrevor.Blocks.ItemFeatures =  (function(){
   });
 })();;
 
-Spotlight.onLoad(function(){
-  var indicators = $('.item-features .slideshow-indicators li');
-  indicators.each(function(){
-    $(this).on('click', function(){
-      indicators.removeClass("active");
-      $(this).addClass("active");
+
+(function($){
+  // Plugin definition
+  $.fn.featuredItemsCarousel = function(options) {
+    var indicators = $(this).find('.slideshow-indicators li');
+
+    indicators.each(function(){
+      $(this).on('click', function(){
+        indicators.removeClass("active");
+        $(this).addClass("active");
+      });
     });
+  };
+
+  Spotlight.onLoad(function(){
+    $('.item-features').featuredItemsCarousel();
   });
-});
+})(jQuery);
