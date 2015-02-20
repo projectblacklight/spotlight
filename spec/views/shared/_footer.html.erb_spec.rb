1029e1e5e22f93660973e579ec04a54c6508790f
Chris Beer <cabeer@stanford.edu>
2015-02-20 12:58:38 -0800

    Add analytics partial to the footer

--- spec/views/shared/_footer.html.erb_spec.rb
+++ spec/views/shared/_footer.html.erb_spec.rb
@@ -3,9 +3,18 @@ require 'spec_helper'
 module Spotlight
   describe "shared/_footer", :type => :view do
     let(:current_exhibit) { double(title: "Some title", subtitle: "Subtitle") }
-  
-    it "should display social media links" do
+
+    before do
       allow(view).to receive_messages(current_exhibit: current_exhibit)
+    end
+
+    it "should include analytics reporting" do
+      stub_template "shared/_analytics.html.erb" => "analytics"
+      render
+      expect(rendered).to have_content "analytics"
+    end
+
+    it "should display social media links" do
       render
       expect(rendered).to have_selector('footer .social-share-button a.social-share-button-twitter[title="Twitter"]')
       expect(rendered).to have_selector('footer .social-share-button a.social-share-button-facebook[title="Facebook"]')
