c0f390ed38d455ce38c2f260ee274e839c738027
Jessie Keck <jessie.keck@gmail.com>
2015-02-25 12:09:42 -0800

    Refactor contacts cropping into croppable plugin.

---
+++ app/assets/javascripts/spotlight/croppable.js
@@ -0,0 +1,88 @@
+Spotlight.onLoad(function() {
+  $('[data-croppable="true"]').croppable();
+});
+
+
+/*
+  Croppable plugin
+  Implements http://deepliquid.com/content/Jcrop.html
+  Add jcrop data-attributes to file input (with data-croppable='true') to instantiate.
+*/
+
+(function($) {
+  $.fn.croppable = function(opts) {
+    var croppables = this;
+    var opts = opts;
+    var pluginDefults = {
+      setSelect: "[0,0,200,200]"
+    }
+
+    $(croppables).each(function() {
+      var fileUpload = $(this);
+      var cropid = fileUpload.data('selector') || this.id;
+      var cropinfo = $("#" + cropid + "_crop").val();
+      var cropbox = $("#" + cropid + "_cropbox");
+      var previewbox = $("#" + cropid + "_previewbox");
+
+      var defaults = {
+        setSelect: $.parseJSON(cropinfo || pluginDefults['setSelect']),
+        selector: cropid
+      }
+
+      var options = $.extend(defaults, fileUpload.data(), opts);
+
+      function updatePreview(coords) {
+        previewbox.css({
+          width: Math.round(100/coords.w * cropbox.width()) + 'px',
+          height: Math.round(100/coords.h * cropbox.height()) + 'px',
+          marginLeft: '-' + Math.round(100/coords.w * coords.x) + 'px',
+          marginTop: '-' + Math.round(100/coords.h * coords.y) + 'px'
+        });
+      };
+
+      function update(coords) {
+        $('#' + cropid + '_crop_x').val(coords.x);
+        $('#' + cropid + '_crop_y').val(coords.y);
+        $('#' + cropid + '_crop_w').val(coords.w);
+        $('#' + cropid + '_crop_h').val(coords.h);
+        updatePreview(coords);
+      };
+      cropbox.Jcrop(
+
+        $.extend(options, {
+          onSelect: update,
+          onChange: update
+        })
+      );
+
+      fileUpload.on('change', function() {
+
+        var file = this.files[0];
+        var img = cropbox[0];
+
+        img.file = file;
+
+        var reader = new FileReader();
+
+        reader.onload = (function(aImg) { return function(e) {
+          var jcrop_api = cropbox.data('Jcrop');
+          jcrop_api.setImage(e.target.result);
+          cropbox.css({width: "", height: ""});
+          cropbox[0].src = e.target.result;
+          if(previewbox.length > 0) {
+            previewbox[0].src = e.target.result;
+          }
+          jcrop_api.setSelect([0,0,200,200]);
+        }; })(img);
+
+        cropbox.closest('.missing-croppable').removeClass('missing-croppable');
+
+        reader.readAsDataURL(file);
+
+      });
+
+    });
+
+    return this;
+  };
+})(jQuery);
