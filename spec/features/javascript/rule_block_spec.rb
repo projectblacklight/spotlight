cb816565bf4db63c3513f9875f5be213d3fe3d4c
Chris Beer <cabeer@stanford.edu>
2015-02-25 16:19:25 -0800

    Add a horizontal rule block

---
+++ spec/features/javascript/rule_block_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe "Horizontal rule block", type: :feature, js: true do
+  let(:exhibit) { FactoryGirl.create(:exhibit) }
+  let(:exhibit_curator) { FactoryGirl.create(:exhibit_curator, exhibit: exhibit) }
+  let!(:feature_page) { FactoryGirl.create(:feature_page, exhibit: exhibit) }
+  before { login_as exhibit_curator }
+
+  it "should allow the user to select which image in a multi image object to display" do
+    skip("Passing locally but Travis is throwing intermittent errors") if ENV["CI"]
+    exhibit.home_page.content = "[]"
+    exhibit.home_page.save
+
+    visit spotlight.exhibit_home_page_path(exhibit, exhibit.home_page)
+    click_link "Edit"
+
+    add_widget 'rule'
+
+    save_page
+
+    expect(page).to have_css("hr")
+  end
+end
