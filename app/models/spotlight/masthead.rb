fe3196023b25582bf36fdfc2fff1de47b95f4bd6
Jessie Keck <jessie.keck@gmail.com>
2015-02-27 15:51:36 -0800

    Allow for a masthead to be uploaded from an image in the exhibit.

--- app/models/spotlight/masthead.rb
+++ app/models/spotlight/masthead.rb
@@ -6,6 +6,11 @@ class Spotlight::Masthead < ActiveRecord::Base
     recreate_image_versions if image.present?
   end
 
+  def document
+    return unless document_global_id && source == 'exhibit'
+    @document ||= GlobalID::Locator.locate document_global_id
+  end
+
   def display?
     self.display && image.cropped_masthead.present?
   end
