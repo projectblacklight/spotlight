853dd0c313aa990398905abcc528ae854d2528e6
Chris Beer <cabeer@stanford.edu>
2015-03-06 10:34:39 -0800

    Extract block limit and grouped block control plugins for sir trevor

--- app/assets/javascripts/spotlight/pages.js
+++ app/assets/javascripts/spotlight/pages.js
@@ -2,33 +2,6 @@
 // All this logic will automatically be available in application.js.
 Spotlight.onLoad(function(){
 
-  SirTrevor.BlockControls.prototype.initialize = function() {
-    var groups = {};
-    for(var block_type in this.available_types) {
-      if (SirTrevor.Blocks.hasOwnProperty(block_type)) {
-        var block_control = new SirTrevor.BlockControl(block_type, this.instance_scope);
-        if (block_control.can_be_rendered) {
-          var blockGroup = SirTrevor.Blocks[block_type].prototype.blockGroup;
-          groups[blockGroup] = groups[blockGroup] || [];
-          groups[blockGroup].push(block_control.render().$el);
-        }
-      }
-    }
-    for(groupKey in groups) {
-      var group   = groups[groupKey];
-      if(groupKey == 'undefined' || groupKey === undefined) {
-        groupKey = "Standard widgets";
-      }
-      var groupEl = $("<div class='st-controls-group'><div class='st-group-control-label'>" + groupKey + "</div></div>");
-      $.each(group, function(){
-        groupEl.append($(this));
-      });
-      this.$el.append(groupEl);
-    }
-
-    this.$el.delegate('.st-block-control', 'click', this.handleControlButtonClick);
-  };
-
   SirTrevor.setDefaults({
     uploadUrl: $('[data-attachment-endpoint]').data('attachment-endpoint')
   });
@@ -36,27 +9,6 @@ Spotlight.onLoad(function(){
   var instance = $('.js-st-instance').first();
 
   if (instance.length) {
-
-    function checkBlockTypeLimitOnAdd(block) {
-      var control = editor.$outer.find("a[data-type='" + block.blockCSSClass() + "']");
-
-      control.toggleClass("disabled", !editor._canAddBlockType(block.class()));
-    }
-
-    function checkGlobalBlockTypeLimit() {
-      // we don't know what type of block was created or removed.. So, try them all.
-
-      $.each(editor.block_manager.blockTypes, function(type) {
-        var block_type = SirTrevor.Blocks[type].prototype;
-
-        var control = editor.$outer.find(".st-block-control[data-type='" + block_type.type + "']");
-        if (editor.block_manager._getBlockTypeLimit(type) < 0) {
-          control.remove();
-        } else {
-          control.toggleClass("disabled", !editor.block_manager.canAddBlockType(type));
-        }
-      });
-    }
 
     var editor = new SirTrevor.Editor({
       el: instance,
@@ -69,9 +21,6 @@ Spotlight.onLoad(function(){
       }
     });
 
-    editor.mediator.on('block:create:new', checkBlockTypeLimitOnAdd);
-    editor.mediator.on('block:remove', checkGlobalBlockTypeLimit);
-
-    checkGlobalBlockTypeLimit();
+    new Spotlight.BlockLimits(editor).enforceLimits();
   }
 });
