abfdea3980b1f6aa21f13e8a58322d018e2c6162
Chris Beer <cabeer@stanford.edu>
2015-03-06 10:09:14 -0800

    Extract previewable and titleable block mixins

--- app/assets/javascripts/spotlight/pages.js
+++ app/assets/javascripts/spotlight/pages.js
@@ -69,61 +69,9 @@ Spotlight.onLoad(function(){
       }
     });
 
-    editor.block_manager.blocks.forEach(injectElementsIntoSirTrevorBlock);
-
-    editor.mediator.on('block:render', injectElementsIntoSirTrevorBlock);
-
     editor.mediator.on('block:create:new', checkBlockTypeLimitOnAdd);
     editor.mediator.on('block:remove', checkGlobalBlockTypeLimit);
 
     checkGlobalBlockTypeLimit();
   }
 });
-
-function injectElementsIntoSirTrevorBlock(block) {
-  addTitleToSirTrevorBlock(block);
-  addPreviewToSirTrevorBlock(block);
-};
-
-function addTitleToSirTrevorBlock(block){
-  block.$inner.append("<div class='st-title'>" + block.title() + "</div>");
-};
-
-function addPreviewToSirTrevorBlock(block){
-  var preview = $('<button class="st-block-ui-btn preview-btn">Preview</button>');
-
-  preview.on('click', function(event) {
-    event.stopPropagation();
-    var preview_btn = $(this);
-    preview_btn.attr('disabled', 'disabled');
-
-    $.post($(this).closest('form').data('preview-url'), {block: JSON.stringify(block.getData()) },
-      function(preview) {
-        var btn = $('<button class="st-block-ui-btn preview-exit-btn">Edit</button>').click(function(event) {
-          event.stopPropagation();
-          block.$inner.show();
-          $(this).closest('.preview').remove();
-          preview_btn.removeAttr('disabled');
-        });
-
-        var widget_bar = $('<div class="st-block__ui" />').append(btn);
-
-        var inner = $('<div class="preview clearfix st-block__inner">');
-        inner.append(preview).append(widget_bar).insertAfter(block.$inner);
-        block.$inner.hide();
-
-        // trigger js behaviors we know blocks can provide
-        $(inner).find('[data-ride="carousel"]').each(function () {
-          var $carousel = $(this)
-          $carousel.carousel($carousel.data())
-        });
-
-        $(inner).find('.slideshow-block').slideshowBlock();
-        $(inner).find('.item-features').featuredItemsCarousel();
-        $(inner).find('[data-embed-url]').oEmbed();
-
-    });
-  });
-
-  block.$inner.find('.st-block__ui').prepend(preview);
-};
