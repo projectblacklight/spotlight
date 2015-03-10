18154f39916719a92f87afba9aeaf9f5b0a3b32f
Chris Beer <cabeer@stanford.edu>
2015-03-10 13:35:28 -0700

    Trigger carousel behavior on block preview and update features block to use carousel behaviors

--- app/assets/javascripts/spotlight/blocks/solr_documents_features_block.js
+++ app/assets/javascripts/spotlight/blocks/solr_documents_features_block.js
@@ -10,6 +10,35 @@ SirTrevor.Blocks.SolrDocumentsFeatures = (function(){
     icon_name: "item_features",
     blockGroup: 'Exhibit item widgets',
     description: "This widget displays items in features",
+    
+    afterPreviewLoad: function(options) {
+      this.$el.find('.carousel').carousel();
+
+      // the bootstrap carousel only initializes data-slide widgets on page load, so we need
+      // to initialize them ourselves..
+      var clickHandler = function (e) {
+        var href
+        var $this   = $(this)
+        var $target = $($this.attr('data-target') || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '')) // strip for ie7
+        if (!$target.hasClass('carousel')) return
+        var options = $.extend({}, $target.data(), $this.data())
+        var slideIndex = $this.attr('data-slide-to')
+        if (slideIndex) options.interval = false
+
+        $.fn.carousel.call($target, options)
+
+        if (slideIndex) {
+          $target.data('bs.carousel').to(slideIndex)
+        }
+
+        e.preventDefault()
+      }
+
+      this.$el.find('.carousel')
+        .on('click.bs.carousel.data-api', '[data-slide]', clickHandler)
+        .on('click.bs.carousel.data-api', '[data-slide-to]', clickHandler)
+    }
+
   });
 
 })();
