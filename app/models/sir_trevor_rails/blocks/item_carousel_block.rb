0414116312807623c5bfac952e22b158982c28cb
Chris Beer <cabeer@stanford.edu>
2015-03-18 12:27:50 -0700

    Remove unused sir-trevor block models

--- app/models/sir_trevor_rails/blocks/item_carousel_block.rb
+++
@@ -1,9 +0,0 @@
-module SirTrevorRails::Blocks
-  class ItemCarouselBlock < SirTrevorRails::Block
-    include SolrDocumentBlock
-
-    def max_height
-      as_json[:data].find { |k,v| k =~ /max-height/ }.try(:last)
-    end
-  end
-end
