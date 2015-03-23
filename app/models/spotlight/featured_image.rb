fa3486893d65858dbd6acd08ce47c36abb8dd260
Chris Beer <cabeer@stanford.edu>
2015-03-22 22:31:00 -0700

    Updated Spotlight::FeaturedImage and Masthead to work with uploaded resources with root-relative URIs to images

--- app/models/spotlight/featured_image.rb
+++ app/models/spotlight/featured_image.rb
@@ -1,7 +1,13 @@
 module Spotlight
   class FeaturedImage < ActiveRecord::Base
     mount_uploader :image, Spotlight::FeaturedImageUploader
-    
+
+    before_validation do
+      if self.document and self.document.uploaded_resource?
+        self.image = self.document.uploaded_resource.url.file
+      end
+    end
+
     after_save do
       if image.present?
         image.cache! if !image.cached?
@@ -9,9 +15,23 @@ module Spotlight
         recreate_image_versions
       end
     end
-    
+
+    def remote_image_url= url
+      # if the image is local, this step will fail.. 
+      # hopefully it's local because it's an uploaded resource, and we'll
+      # catch is in before_validation..
+      unless url.starts_with? "/"
+        super url
+      end
+    end
+
     def document
       return unless document_global_id && source == 'exhibit'
+
+      if @document and document_global_id != @document.to_global_id.to_s
+        @document = nil
+      end
+
       @document ||= GlobalID::Locator.locate document_global_id
     end
   end
---
+++ spec/models/spotlight/featured_image_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Spotlight::FeaturedImage do
+  context "with an uploaded resource" do
+    subject { described_class.new }
+    let(:document) { double(uploaded_resource?: true) }
+
+    it "should replace the remote_image_url fragment with the local path to the file" do
+      allow(document).to receive_message_chain(:uploaded_resource, :url, file: File.open(File.join(FIXTURES_PATH, "avatar.png")))
+      allow(subject).to receive(:document).and_return(document)
+
+      subject.remote_image_url = "/some/path"
+      expect(subject.remote_image_url).to be_nil
+
+      subject.validate
+
+      expect(subject.image.filename).to eq 'avatar.png'
+    end
+  end
+end
