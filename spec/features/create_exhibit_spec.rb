d93ebf54ad350da3963f9f397ddd49ec51894a25
Chris Beer <cabeer@stanford.edu>
2015-11-17 15:30:16 -0800

    Use a simplified form for initially creating an exhibit

--- spec/features/create_exhibit_spec.rb
+++ spec/features/create_exhibit_spec.rb
@@ -16,14 +16,6 @@ describe 'Create a new exhibit', type: :feature do
     expect(page).to have_selector 'h1 small', text: 'Create a new exhibit'
   end
 
-  it 'has a contact emails field' do
-    visit '/'
-    within '.dropdown-menu' do
-      click_link 'Create Exhibit'
-    end
-    expect(page).to have_css('#exhibit_contact_emails_attributes_0_email')
-  end
-
   it 'allows admins to create a new exhibit' do
     visit '/'
     within '.dropdown-menu' do
@@ -31,8 +23,6 @@ describe 'Create a new exhibit', type: :feature do
     end
 
     fill_in 'Title', with: 'My exhibit title'
-    fill_in 'Subtitle', with: 'Some subtitle'
-    fill_in 'Description', with: 'A short description of the exhibit'
 
     click_button 'Save'
 
