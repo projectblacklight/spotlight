afc17e8ec62a66ce5f3204ef7eab46e01b97dffc
Chris Beer <cabeer@stanford.edu>
2015-11-17 15:30:33 -0800

    Exhibits should default to unpublished

--- spec/factories/exhibits.rb
+++ spec/factories/exhibits.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :exhibit, class: Spotlight::Exhibit do
     sequence(:title) { |n| "Exhibit Title #{n}" }
+    published true
     after(:build) { |exhibit| exhibit.searches << FactoryGirl.build(:default_search) }
   end
 end
