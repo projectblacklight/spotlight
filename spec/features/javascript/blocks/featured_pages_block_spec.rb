ba850ea71592176b658484f60ba4d244160fe78b
Jessie Keck <jessie.keck@gmail.com>
2015-11-17 17:06:40 -0800

    Trigger change event on nestable element w/i ST blocks when a new item panel is added.
    We use the change event to re-weight the items but. Drag/Drop triggers the change event but not adding a new item.

--- spec/features/javascript/blocks/featured_pages_block_spec.rb
+++ spec/features/javascript/blocks/featured_pages_block_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
-describe 'Featured Pages Blocks', type: :feature do
+describe 'Featured Pages Blocks', type: :feature, js: true do
   let(:exhibit) { FactoryGirl.create(:default_exhibit) }
-  let!(:feature_page) do
+  let!(:feature_page1) do
     FactoryGirl.create(
       :feature_page,
-      title: 'xyz',
+      title: 'FeaturePage1 Title',
+      weight: 1,
+      exhibit: exhibit
+    )
+  end
+  let!(:feature_page2) do
+    FactoryGirl.create(
+      :feature_page,
+      title: 'FeaturePage2 Title',
+      weight: 0,
       exhibit: exhibit
     )
   end
@@ -16,17 +25,35 @@ describe 'Featured Pages Blocks', type: :feature do
     login_as exhibit_curator
   end
 
-  it 'saves the selected exhibits', js: true do
+  it 'saves the selected exhibits' do
     visit spotlight.exhibit_home_page_path(exhibit, exhibit.home_page)
 
     click_link('Edit')
 
     add_widget 'featured_pages'
 
-    fill_in_typeahead_field with: 'xyz'
+    fill_in_typeahead_field with: feature_page2.title
 
     save_page
 
-    expect(page).to have_content 'xyz'
+    expect(page).to have_content feature_page2.title
+  end
+
+  it 'persists the user selected sort order' do
+    visit spotlight.exhibit_home_page_path(exhibit, exhibit.home_page)
+
+    click_link('Edit')
+
+    add_widget 'featured_pages'
+
+    fill_in_typeahead_field with: feature_page1.title
+    fill_in_typeahead_field with: feature_page2.title
+
+    save_page
+
+    feature_page1_position = page.body =~ /<p class="category-title">\s+#{feature_page1.title}/
+    feature_page2_position = page.body =~ /<p class="category-title">\s+#{feature_page2.title}/
+
+    expect(feature_page1_position).to be < feature_page2_position
   end
 end
