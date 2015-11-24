5f74cabc57a70abe23cb8a9c7e5654ac944fffd1
Chris Beer <cabeer@stanford.edu>
2015-11-24 10:27:33 -0800

    Add migration to remove unused default column from spotlight_exhibits

---
+++ db/migrate/20151124101123_remove_default_from_spotlight_exhibit.rb
@@ -0,0 +1,13 @@
+class RemoveDefaultFromSpotlightExhibit < ActiveRecord::Migration
+  def up
+    return unless Spotlight::Exhibit.column_names.include? :default
+
+    remove_column :spotlight_exhibits, :default
+    remove_index :spotlight_exhibits, :default
+  end
+  
+  def down
+    add_column :spotlight_exhibits, :default, :boolean, default: true
+    add_index :spotlight_exhibits, :default, unique: true
+  end
+end
