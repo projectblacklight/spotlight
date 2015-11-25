e368a984ea6cef248526dac347db99f4816a7f5c
Chris Beer <cabeer@stanford.edu>
2015-11-25 09:15:05 -0800

    Add easy accessors for reindexing all exhibit-specific information into solr documents

--- app/models/concerns/spotlight/solr_document/finder.rb
+++ app/models/concerns/spotlight/solr_document/finder.rb
@@ -17,6 +17,20 @@ module Spotlight
           @index ||= blacklight_config.repository_class.new(blacklight_config)
         end
 
+        def find_each
+          return to_enum(:find_each) unless block_given?
+
+          start = 0
+          search_params = { q: '*:*', fl: 'id', facet: false }
+          response = index.search(search_params.merge(start: start))
+
+          while response.documents.present?
+            response.documents.each { |x| yield x }
+            start += response.documents.length
+            response = index.search(search_params.merge(start: start))
+          end
+        end
+
         protected
 
         def blacklight_config
