77f4bced7489766c9bffa8603d196e77569b9dc1
Chris Beer <cabeer@stanford.edu>
2015-11-27 08:34:13 -0800

    Update factories to be shareable with downstream consumers

--- spec/factories/featured_images.rb
+++ spec/factories/featured_images.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :featured_image, class: Spotlight::FeaturedImage do
-    image { Rack::Test::UploadedFile.new(File.join(FIXTURES_PATH, 'avatar.png')) }
+    image { Rack::Test::UploadedFile.new(File.expand_path(File.join('..', 'fixtures', 'avatar.png'), __dir__)) }
   end
 
   factory :masthead, class: Spotlight::Masthead do
-    image { Rack::Test::UploadedFile.new(File.join(FIXTURES_PATH, 'avatar.png')) }
+    image { Rack::Test::UploadedFile.new(File.expand_path(File.join('..', 'fixtures', 'avatar.png'), __dir__)) }
   end
 end
