c438a8f8e05e4d091d1edbe97cdc7f4758e53ab9
Ronan McHugh <mchugh.r@gmail.com>
2015-12-14 13:33:16 +0100

    move before filter into method

--- lib/spotlight/catalog.rb
+++ lib/spotlight/catalog.rb
@@ -11,17 +11,20 @@ module Spotlight
     include Spotlight::Catalog::AccessControlsEnforcement
 
     included do
-      before_filter do
-        if current_exhibit && can?(:curate, current_exhibit)
-          blacklight_config.add_facet_field 'exhibit_visibility',
-                                            label: I18n.t(:'spotlight.catalog.facets.exhibit_visibility.label'),
-                                            query: {
-                                              private: {
-                                                label: I18n.t(:'spotlight.catalog.facets.exhibit_visibility.private'),
-                                                fq: "#{Spotlight::SolrDocument.visibility_field(current_exhibit)}:false" }
-                                            }
-        end
-      end
+      before_action :add_facet_visibility_field
+    end
+
+    # Adds a facet to display document visibility for the current exhibit
+    # if the user is a curator
+    def add_facet_visibility_field
+      return unless current_exhibit && can?(:curate, current_exhibit)
+      blacklight_config.add_facet_field 'exhibit_visibility',
+                                        label: I18n.t(:'spotlight.catalog.facets.exhibit_visibility.label'),
+                                        query: {
+                                          private: {
+                                            label: I18n.t(:'spotlight.catalog.facets.exhibit_visibility.private'),
+                                            fq: "#{Spotlight::SolrDocument.visibility_field(current_exhibit)}:false" }
+                                        }
     end
   end
 end
