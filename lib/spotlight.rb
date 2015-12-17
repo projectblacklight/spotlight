88b0fc11f8e4cc5794a8ad5d938afc886b65ce5b
Justin Coyne <jcoyne@justincoyne.com>
2015-12-17 11:02:53 +0100

    Reorganize lib/spotlight to move classes to ./app/ as appropriate

--- lib/spotlight.rb
+++ lib/spotlight.rb
@@ -4,8 +4,4 @@ require 'spotlight/engine'
 ##
 # Spotlight
 module Spotlight
-  autoload :Config, 'spotlight/config'
-  autoload :Base, 'spotlight/base'
-  autoload :Controller, 'spotlight/controller'
-  autoload :Catalog, 'spotlight/catalog'
 end
