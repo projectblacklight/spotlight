5517b624199a9bd38074b5f5bd7b293501a0dba4
Chris Beer <cabeer@stanford.edu>
2016-07-08 11:53:38 -0700

    Provide default images for all image uploaders

--- app/uploaders/spotlight/avatar_uploader.rb
+++ app/uploaders/spotlight/avatar_uploader.rb
@@ -16,5 +16,9 @@ module Spotlight
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
+
+    def default_url
+      ActionController::Base.helpers.asset_path('fallback/' + [version_name, 'default.png'].compact.join('_'))
+    end
   end
 end
