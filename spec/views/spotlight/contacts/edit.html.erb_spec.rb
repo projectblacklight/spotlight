dfa8e109b7ef7cbfa710088f7bed3b36c71aaa27
Justin Coyne <jcoyne@justincoyne.com>
2016-08-02 09:21:20 -0500

    Add IIIF crop for contacts
    Fixes #1538

--- spec/views/spotlight/contacts/edit.html.erb_spec.rb
+++ spec/views/spotlight/contacts/edit.html.erb_spec.rb
@@ -9,6 +9,9 @@ describe 'spotlight/contacts/edit.html.erb' do
   before do
     allow(view).to receive(:exhibit_contacts_path).and_return('/exhibit/1/contacts')
     allow(view).to receive(:exhibit_about_pages_path).and_return('/exhibit/admin/about')
+    allow(view).to receive(:featured_images_path).and_return('/featured_images')
+    allow(view).to receive(:contact_crop_options).and_return(croppable: true,
+                                                             selector: 'featuredimage_image')
     assign(:contact, contact)
     assign(:exhibit, exhibit)
     allow(view).to receive(:current_exhibit).and_return(exhibit)
