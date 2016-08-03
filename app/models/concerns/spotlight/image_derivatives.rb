96064acc165b9340103b113292d736eb8a2df608
Justin Coyne <jcoyne@justincoyne.com>
2016-08-03 07:52:13 -0500

    Remove image processing lambdas for uploaded items

--- app/models/concerns/spotlight/image_derivatives.rb
+++ app/models/concerns/spotlight/image_derivatives.rb
@@ -1,35 +1,14 @@
 module Spotlight
   ###
-  # Include Spotlight::ImageDerivatives in a class or module to get
-  # the derivative configurations in #spotlight_image_derivatives.
+  # Stores the derivative configurations in #spotlight_image_derivatives.
   # A new derivative could theoretically be added by putting the
   # following in an initializer.
   # Spotlight::ImageDerivatives.spotlight_image_derivatives << {
   #   version: :my_version,
-  #   field: :my_field,
-  #   lambda: lambda {|_|
-  #     version :my_version do
-  #       process :resize_to_fill => [30,30]
-  #     end
-  #   }
+  #   field: :my_field
   # }
-  #
-  # This will then create that new CarrierWave version in any class that extends this module
-  # and calls the apply_spotlight_image_derivative_versions class method described below.
   module ImageDerivatives
     mattr_accessor :spotlight_image_derivatives
-
-    # Extend Spotlight::ImageDerivatives in a CarrierWave uploader
-    # then you can call this as a class method and all of the
-    # configured versions will be available
-    def apply_spotlight_image_derivative_versions
-      spotlight_image_derivatives.each do |version_config|
-        if (c = version_config[:lambda]).present?
-          class_eval(&c)
-        end
-      end
-    end
-
     # Set default derivative configurations
     self.spotlight_image_derivatives ||= [
       {
@@ -37,21 +16,11 @@ module Spotlight
       },
       {
         version: :thumb,
-        field: Spotlight::Engine.config.try(:thumbnail_field),
-        lambda: lambda do |_|
-          version :thumb do
-            process resize_to_fit: [400, 400]
-          end
-        end
+        field: Spotlight::Engine.config.try(:thumbnail_field)
       },
       {
         version: :square,
-        field: Spotlight::Engine.config.try(:square_image_field),
-        lambda: lambda do |_|
-          version :square do
-            process resize_to_fill: [100, 100]
-          end
-        end
+        field: Spotlight::Engine.config.try(:square_image_field)
       }
     ].reject { |v| v[:field].blank? }
   end
--- app/services/spotlight/carrierwave_file_resolver.rb
+++ app/services/spotlight/carrierwave_file_resolver.rb
@@ -2,6 +2,7 @@ module Spotlight
   # Used by RIIIF to find files uploaded by carrierwave
   class CarrierwaveFileResolver < Riiif::AbstractFileSystemResolver
     def pattern(id)
+      # TODO: need to make Resource::Upload be a kind of FeaturedImage.
       uploaded_file = Spotlight::FeaturedImage.find(id).image.file
       raise Riiif::ImageNotFoundError, "unable to find file for #{id}" if uploaded_file.nil?
       uploaded_file.file
