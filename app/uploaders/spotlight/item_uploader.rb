f0eb176ca009b997b8074e0dc7a97bec9eb1ba2a
Justin Coyne <jcoyne@justincoyne.com>
2016-08-02 21:16:22 -0500

    Remove default_url from uploaders

--- app/uploaders/spotlight/item_uploader.rb
+++ app/uploaders/spotlight/item_uploader.rb
@@ -17,9 +17,5 @@ module Spotlight
     def store_dir
       "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
     end
-
-    def default_url
-      ActionController::Base.helpers.image_path('spotlight/fallback/' + [version_name, 'default.png'].compact.join('_'))
-    end
   end
 end
