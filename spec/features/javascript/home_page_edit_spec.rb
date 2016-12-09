41e43963de6cf59aadc37898bdd8360b50374383
Tommy Ingulfsen <tommyi@stanford.edu>
2016-12-09 15:17:09 -0800

    Update Sir Trevor blocks.

--- spec/features/javascript/home_page_edit_spec.rb
+++ spec/features/javascript/home_page_edit_spec.rb
@@ -10,4 +10,23 @@ feature 'Editing the Home Page', js: true do
     expect(page).to have_css("[data-type='solr_documents']", visible: true)
     expect(page).not_to have_css("[data-type='search_results']", visible: true)
   end
+
+  it 'correctly saves a list widget' do
+    visit spotlight.edit_exhibit_home_page_path(exhibit)
+    click_add_widget
+    expect(page).to have_css('button.st-block-controls__button')
+
+    find("button[data-type='list']").click
+    expect(page).to have_css('ul.st-list-block__list')
+    expect(page).to have_css('li.st-list-block__item')
+    expect(page).to have_css('*[contenteditable=true]')
+    expect(page).to have_css('div.st-list-block__editor[contenteditable=true]', count: 1)
+
+    first_element = page.all('div.st-list-block__editor[contenteditable=true]').first
+    first_element.set('one')
+
+    click_button 'Save changes'
+    expect(page).to have_css('div.st__content-block--list ul li', count: 1)
+    expect(page).to have_css('div.st__content-block--list ul li', text: 'on')
+  end
 end
