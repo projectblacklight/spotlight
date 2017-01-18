e7ee4cae0093d859f75322f76c34dc9afc1717ae
Johnathan Martin <john.martin@stanford.edu>
2017-01-17 19:10:37 -0800

    helper methods for setting job status, tests
    * ReindexingLogEntry #reindexing_started!, #reindexing_succeeded!, #reindexing_failed!
    * tests for ReindexingLogEntry status update methods

--- spec/models/spotlight/reindexing_log_entry_spec.rb
+++ spec/models/spotlight/reindexing_log_entry_spec.rb
@@ -52,4 +52,84 @@ describe Spotlight::ReindexingLogEntry, type: :model do
       end
     end
   end
+
+  describe 'state updating methods' do
+    describe '#in_progress!' do
+      subject { FactoryGirl.build(:unstarted_reindexing_log_entry) }
+
+      context 'executes normally' do
+        it 'sets start_time and job_status' do
+          lower_time_bound = Time.zone.now
+          subject.in_progress!
+          upper_time_bound = Time.zone.now
+
+          expect(subject.start_time).to be_between(lower_time_bound, upper_time_bound)
+          expect(subject.job_status).to eq 'in_progress'
+        end
+      end
+
+      context 'encounters an unexpected error' do
+        it "traps the exception and logs an error so that the caller doesn't have to deal with it" do
+          expect(subject).to receive(:'start_time=').and_raise StandardError.new # try to blow up the in_progress! call
+          expect(Rails.logger).to receive(:error) do |arg|
+            expect(arg).to match(/^unexpected error updating log entry to :in_progress from \[".*reindexing_log_entry.rb/)
+          end
+
+          expect { subject.in_progress! }.not_to raise_error
+        end
+      end
+    end
+
+    describe '#succeeded!' do
+      subject { FactoryGirl.build(:in_progress_reindexing_log_entry) }
+
+      context 'executes normally' do
+        it 'sets end_time and job_status' do
+          lower_time_bound = Time.zone.now
+          subject.succeeded!
+          upper_time_bound = Time.zone.now
+
+          expect(subject.end_time).to be_between(lower_time_bound, upper_time_bound)
+          expect(subject.job_status).to eq 'succeeded'
+        end
+      end
+
+      context 'encounters an unexpected error' do
+        it "traps the exception and logs an error so that the caller doesn't have to deal with it" do
+          expect(subject).to receive(:'end_time=').and_raise StandardError.new # try to blow up the succeeded! call
+          expect(Rails.logger).to receive(:error) do |arg|
+            expect(arg).to match(/^unexpected error updating log entry to :succeeded from \[".*reindexing_log_entry.rb/)
+          end
+
+          expect { subject.succeeded! }.not_to raise_error
+        end
+      end
+    end
+
+    describe '#failed!' do
+      subject { FactoryGirl.build(:in_progress_reindexing_log_entry) }
+
+      context 'executes normally' do
+        it 'sets end_time and job_status' do
+          lower_time_bound = Time.zone.now
+          subject.failed!
+          upper_time_bound = Time.zone.now
+
+          expect(subject.end_time).to be_between(lower_time_bound, upper_time_bound)
+          expect(subject.job_status).to eq 'failed'
+        end
+      end
+
+      context 'encounters an unexpected error' do
+        it "traps the exception and logs an error so that the caller doesn't have to deal with it" do
+          expect(subject).to receive(:'end_time=').and_raise StandardError.new # try to blow up the failed! call
+          expect(Rails.logger).to receive(:error) do |arg|
+            expect(arg).to match(/^unexpected error updating log entry to :failed from \[".*reindexing_log_entry.rb/)
+          end
+
+          expect { subject.failed! }.not_to raise_error
+        end
+      end
+    end
+  end
 end
