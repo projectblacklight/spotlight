804ec71f10670b03df4990ab119593e1407b1a26
Chris Beer <chris@cbeer.info>
2017-08-02 14:16:50 -0700

    belongs_to fixes

--- spec/features/tags_admin_spec.rb
+++ spec/features/tags_admin_spec.rb
@@ -1,6 +1,6 @@
 describe 'Tags Administration', type: :feature do
   let(:exhibit) { FactoryGirl.create(:exhibit) }
-  let!(:tagging) { FactoryGirl.create(:tagging, tagger: exhibit) }
+  let!(:tagging) { FactoryGirl.create(:tagging, tagger: exhibit, taggable: exhibit) }
   let(:exhibit_curator) { FactoryGirl.create(:exhibit_curator, exhibit: exhibit) }
   before { login_as exhibit_curator }
 
