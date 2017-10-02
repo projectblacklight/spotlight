45994adc83434a9bda66e34d3445c490eb3a93b5
Justin Coyne <jcoyne@justincoyne.com>
2017-10-02 11:26:48 -0500

    CarrierwaveFileResolver does not need a base_path

--- app/services/spotlight/carrierwave_file_resolver.rb
+++ app/services/spotlight/carrierwave_file_resolver.rb
@@ -1,6 +1,11 @@
 module Spotlight
   # Used by RIIIF to find files uploaded by carrierwave
   class CarrierwaveFileResolver < Riiif::AbstractFileSystemResolver
+    # Override initialzer to avoid deprecation about not providing base path
+    def initializer
+      # nop
+    end
+
     def pattern(id)
       uploaded_file = Spotlight::FeaturedImage.find(id).image.file
       raise Riiif::ImageNotFoundError, "unable to find file for #{id}" if uploaded_file.nil?
