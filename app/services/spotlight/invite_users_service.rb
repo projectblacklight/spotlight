d0bcb7f684a26959f7319e4bfc4e6311e5797248
Jessie Keck <jessie.keck@gmail.com>
2017-11-30 11:05:04 -0800

    Add a service to invite the users of a resource (site/exhibit currently) who were created via an invite but an invitation has never been sent to them.

---
+++ app/services/spotlight/invite_users_service.rb
@@ -0,0 +1,15 @@
+module Spotlight
+  ##
+  # A simple service to invite any users who where created by an invitation but it was never sent.
+  # This is done because the associated information between the resource, the role, and the user
+  # need to be persisted in order to generate the appropriate content in the invitation email.
+  class InviteUsersService
+    def self.call(resource:)
+      resource.roles.includes(:user).each do |role|
+        user = role.user
+
+        user.deliver_invitation if user.created_by_invite? && user.invitation_sent_at.blank?
+      end
+    end
+  end
+end
---
+++ spec/services/spotlight/invite_users_service_spec.rb
@@ -0,0 +1,45 @@
+describe Spotlight::InviteUsersService do
+  subject { described_class.call(resource: resource) }
+
+  let(:resource) do
+    FactoryBot.create(:exhibit, roles: [FactoryBot.create(:role, user: user)])
+  end
+
+  context 'when the user was not created by an invite' do
+    let(:user) { FactoryBot.create(:user) }
+
+    it 'does not send an invite' do
+      expect do
+        subject
+      end.to change { Devise::Mailer.deliveries.count }.by(0)
+      expect(user.reload.invitation_sent_at).to be_nil
+    end
+  end
+
+  context 'when the user has already received an invite' do
+    let!(:user) do
+      User.invite!(email: 'a-user-that-does-not-exist@example.com', skip_invitation: true).tap do |u|
+        u.invitation_sent_at = Time.zone.now
+      end
+    end
+
+    it 'does not send an invite' do
+      expect do
+        subject
+      end.to change { Devise::Mailer.deliveries.count }.by(0)
+    end
+  end
+
+  context 'when the user was created by but not yet received an invite' do
+    let(:user) do
+      User.invite!(email: 'a-user-that-does-not-exist@example.com', skip_invitation: true)
+    end
+
+    it 'sends an invite' do
+      expect do
+        subject
+      end.to change { Devise::Mailer.deliveries.count }.by(1)
+      expect(user.reload.invitation_sent_at).to be_present
+    end
+  end
+end
