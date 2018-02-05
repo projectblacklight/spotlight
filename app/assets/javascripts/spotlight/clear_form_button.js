0f714c75c569aac9ef0167a433bf7e05307e19e5
Camille Villa <cvilla100@gmail.com>
2018-02-04 19:20:38 -0800

    Refactor jQuery

--- app/assets/javascripts/spotlight/clear_form_button.js
+++ app/assets/javascripts/spotlight/clear_form_button.js
@@ -4,18 +4,24 @@ Spotlight.onLoad(function() {
 
 (function($) {
   $.fn.ClearFormButton = function() {
-    var clearBtn = this;
-    var input = $(clearBtn).parent().prev('input');
-    $(input).on('keyup', function() {
-      if (input.val() !== '') {
-        $(clearBtn).css('display', 'inline-block');
+    var $clearBtn = $(this);
+    var $input = $clearBtn.parent().prev('input');
+    var btnCheck = function(){
+      if ($input.val() !== '') {
+        $clearBtn.css('display', 'inline-block');
       } else {
-        $(clearBtn).css('display', 'none');
+        $clearBtn.css('display', 'none');
       }
+    };
+
+    btnCheck();
+    $input.on('keyup', function() {
+      btnCheck();
     });
-    $(clearBtn).on('click', function(event) {
+
+    $clearBtn.on('click', function(event) {
       event.preventDefault();
-      input.val('');
+      $input.val('');
     });
   };
 })(jQuery);
