e29fa3ade043863639622ac0d0a488c9cc5ac3b6
Jessie Keck <jessie.keck@gmail.com>
2018-03-12 14:14:46 -0700

    Add a locale picker into the user util links.
    This locale picker allows a curator/admin of an exhibit to view any
    locales that have been added while a visitor can only see public locales.

--- app/models/spotlight/language.rb
+++ app/models/spotlight/language.rb
@@ -3,5 +3,13 @@ module Spotlight
   class Language < ActiveRecord::Base
     belongs_to :exhibit
     validates :locale, presence: true
+
+    def to_native
+      Spotlight::Engine.config.i18n_locales[locale.downcase.to_sym] || ''
+    end
+
+    def self.default_instance
+      new(locale: I18n.default_locale)
+    end
   end
 end
