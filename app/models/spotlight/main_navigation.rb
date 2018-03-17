fd0aaf307bd61d91a683229233f97b93e6f3a853
Jack Reed <phillipjreed@gmail.com>
2018-03-16 18:08:14 -0600

    adds main menu navigation translation functionality

--- app/models/spotlight/main_navigation.rb
+++ app/models/spotlight/main_navigation.rb
@@ -2,11 +2,15 @@ module Spotlight
   ##
   # Exhibit navbar links
   class MainNavigation < ActiveRecord::Base
+    include Spotlight::Translatables
+
     belongs_to :exhibit, touch: true
     default_scope { order('weight ASC') }
     scope :browse, -> { find_by(nav_type: 'browse') }
     scope :about, -> { find_by(nav_type: 'about') }
+    scope :curated_features, -> { find_by(nav_type: 'curated_features') }
     scope :displayable, -> { where(display: true) }
+    translates :label
 
     def displayable?
       display?
@@ -22,6 +26,14 @@ module Spotlight
 
     def default_label
       I18n.t(:"spotlight.main_navigation.#{nav_type}")
+    end
+
+    private
+
+    ##
+    # Allows us to scope translations namespace.
+    def slug
+      ['main_navigation', nav_type].join('.')
     end
   end
 end
