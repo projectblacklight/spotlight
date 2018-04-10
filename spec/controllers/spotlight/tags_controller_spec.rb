15598247641e7c988eab632611ac663635361c52
Chris Beer <cabeer@stanford.edu>
2018-04-10 11:29:55 -0700

    Move tags feature tests into controller + view tests

--- spec/controllers/spotlight/tags_controller_spec.rb
+++ spec/controllers/spotlight/tags_controller_spec.rb
@@ -2,6 +2,10 @@ describe Spotlight::TagsController, type: :controller do
   routes { Spotlight::Engine.routes }
   let(:exhibit) { FactoryBot.create(:exhibit) }
 
+  before do
+    exhibit.tag(SolrDocument.new(id: 'x').sidecar(exhibit), with: 'paris, normandy', on: :tags)
+  end
+
   describe 'when not signed in' do
     describe 'GET index' do
       it 'redirects to sign inl' do
@@ -19,7 +23,8 @@ describe Spotlight::TagsController, type: :controller do
         expect(controller).to receive(:add_breadcrumb).with('Tags', exhibit_tags_path(exhibit))
         get :index, params: { exhibit_id: exhibit }
         expect(response).to be_successful
-        expect(assigns[:tags]).to eq []
+        expect(assigns[:tags].length).to eq 2
+        expect(assigns[:tags].map(&:name)).to match_array %w[paris normandy]
         expect(assigns[:exhibit]).to eq exhibit
       end
 
