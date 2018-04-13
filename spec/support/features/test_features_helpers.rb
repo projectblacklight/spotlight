6d64acf2c2947a407a9673a311be35c128ee1dde
Chris Beer <cabeer@stanford.edu>
2018-04-13 10:15:30 -0700

    Try using have_selector instead of have_text

--- spec/support/features/test_features_helpers.rb
+++ spec/support/features/test_features_helpers.rb
@@ -36,7 +36,7 @@ module Spotlight
       EOF
       click_button('Save changes')
       # verify that the page was created
-      expect(page).to have_content('page was successfully updated')
+      expect(page).to have_selector('.alert', text: 'page was successfully updated')
     end
 
     RSpec::Matchers.define :have_breadcrumbs do |*expected|
