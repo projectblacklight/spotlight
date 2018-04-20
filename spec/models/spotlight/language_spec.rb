7964503e19fb0f5cf644914e8c8e4b0bff299f82
Jessie Keck <jessie.keck@gmail.com>
2018-04-20 12:01:16 -0700

    Destroy related translated pages when destroying a Language.

--- spec/models/spotlight/language_spec.rb
+++ spec/models/spotlight/language_spec.rb
@@ -10,4 +10,30 @@ RSpec.describe Spotlight::Language do
       expect(described_class.new(locale: 'xx').to_native).to eq ''
     end
   end
+
+  describe 'when being destroyed' do
+    let(:exhibit) { FactoryBot.create(:exhibit) }
+    let(:language) { described_class.create(locale: 'es', exhibit: exhibit) }
+    it 'also destroys its locale related pages' do
+      page_es = exhibit.home_page.clone_for_locale('es').tap(&:save)
+
+      expect(Spotlight::Page.exists?(page_es.id)).to be true
+
+      expect do
+        language.destroy
+      end.to change(Spotlight::Page, :count).by(-1)
+
+      expect(Spotlight::Page.exists?(page_es.id)).to be false
+    end
+
+    it 'also destroys its locale related translations' do
+      translation = FactoryBot.create(:translation, key: 'some.key', exhibit: exhibit, locale: 'es')
+
+      expect do
+        language.destroy
+      end.to change(Translation, :count).by(-1)
+
+      expect(Translation.exists?(translation.id)).to be false
+    end
+  end
 end
