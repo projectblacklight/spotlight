ad15eda7dc0c50c12c874a7ffe2d1acfd0405dae
Chris Beer <cabeer@stanford.edu>
2018-04-26 10:00:18 -0700

    Remove friendly_id deprecation silencing hack

--- spec/support/disable_friendly_id_deprecation_warnings.rb
+++
@@ -1,8 +0,0 @@
-default_deprecation_behaviours = ActiveSupport::Deprecation.behavior
-ActiveSupport::Deprecation.behavior = lambda do |message, callstack|
-  raise 'Remove friendly_id deprecation silencing patch!' if Rails::VERSION::MAJOR == 5 && Rails::VERSION::MINOR > 1
-  unless callstack.find { |l| l.path =~ %r{gems/friendly_id} } &&
-         message =~ /The behavior of .* inside of after callbacks will be changing/
-    default_deprecation_behaviours.each { |b| b.call(message, callstack) }
-  end
-end
