a203cb8ae5f716f8ab337eee1b4fa0d3886a1786
Chris Beer <chris@cbeer.info>
2018-12-18 13:04:20 -0800

    Update solr_document_features_block to correctly fallback to the default title field if a primary caption isn't provided. (#2115)

--- spec/views/spotlight/sir_trevor/blocks/_solr_documents_features_block.html.erb_spec.rb
+++ spec/views/spotlight/sir_trevor/blocks/_solr_documents_features_block.html.erb_spec.rb
@@ -4,14 +4,20 @@ describe 'spotlight/sir_trevor/blocks/_solr_documents_features_block.html.erb', 
     SirTrevorRails::Blocks::SolrDocumentsFeaturesBlock.new({ type: 'block', data: { 'show-primary-caption' => true, 'primary-caption-field' => 'x' } }, page)
   end
 
+  let(:blacklight_config) do
+    Blacklight::Configuration.new do |config|
+      config.index.title_field = 'full_title_tesim'
+    end
+  end
+
   before do
     doc1 = [{}, SolrDocument.new(id: 1, x: 'a' * 100)]
-    doc2 = [{}, SolrDocument.new(id: 2)]
+    doc2 = [{}, SolrDocument.new(id: 2, full_title_tesim: 'full_title')]
     doc3 = [{}, SolrDocument.new(id: 3)]
     allow(block).to receive(:each_document).and_return([doc1, doc2, doc3])
     allow(block).to receive_messages(documents?: true)
     allow(view).to receive_messages(solr_documents_features_block: block)
-    allow(view).to receive_messages(has_thumbnail?: true, render_thumbnail_tag: 'thumb', blacklight_config: Blacklight::Configuration.new)
+    allow(view).to receive_messages(has_thumbnail?: true, render_thumbnail_tag: 'thumb', blacklight_config: blacklight_config)
   end
 
   it 'has a slideshow block' do
@@ -29,5 +35,17 @@ describe 'spotlight/sir_trevor/blocks/_solr_documents_features_block.html.erb', 
     expect(rendered).to have_selector '.carousel-indicators'
     expect(rendered).to have_selector '.carousel-indicators li', count: 3
     expect(rendered).to have_selector '.carousel-indicators li', text: 'a' * 92 + '...'
+  end
+
+  describe 'without a primary caption' do
+    let(:block) do
+      SirTrevorRails::Blocks::SolrDocumentsFeaturesBlock.new({ type: 'block', data: { 'show-primary-caption' => false } }, page)
+    end
+
+    it 'falls back to the regular document title for the caption' do
+      render partial: p, locals: { item_carousel_block: block }
+      expect(rendered).to have_selector '.item-features'
+      expect(rendered).to have_selector '.carousel-indicators li', text: 'full_title'
+    end
   end
 end
