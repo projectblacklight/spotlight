723c828c35ad506c4fb1827d49e92cfc64d23f11
ives1227 <valdeva_crema@harvard.edu>
2019-01-09 12:22:26 -0500

    Updating template to point at this version temporarily.

--- template.rb
+++ template.rb
@@ -7,7 +7,7 @@ spotlight_options = ENV.fetch('SPOTLIGHT_INSTALL_OPTIONS', DEFAULT_SPOTLIGHT_OPT
 
 # Add gem dependencies to the application
 gem 'blacklight', ' ~> 6.0'
-gem 'blacklight-spotlight', ENV['SPOTLIGHT_GEM'] ? { path: ENV['SPOTLIGHT_GEM'] } : { github: 'projectblacklight/spotlight' }
+gem 'blacklight-spotlight', ENV['SPOTLIGHT_GEM'] ? { path: ENV['SPOTLIGHT_GEM'] } : { github: 'harvard-library/spotlight', :branch => 'log_entries_without_iframe_viewer' }
 
 Bundler.with_clean_env do
   run 'bundle install'
