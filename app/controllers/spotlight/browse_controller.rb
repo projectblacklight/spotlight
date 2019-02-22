bdf8634f14aad6232bad6061dd411cde50b80934
Chris Beer <cabeer@stanford.edu>
2019-02-22 12:10:53 -0800

    Blacklight 6 -> 7: Blacklight::JsonPresenter signature changed

--- app/controllers/spotlight/browse_controller.rb
+++ app/controllers/spotlight/browse_controller.rb
@@ -31,7 +31,7 @@ module Spotlight
       respond_to do |format|
         format.html
         format.json do
-          @presenter = Blacklight::JsonPresenter.new(@response, @document_list, facets_from_request, blacklight_config)
+          @presenter = Blacklight::JsonPresenter.new(@response, blacklight_config)
           render template: 'catalog/index'
         end
       end
