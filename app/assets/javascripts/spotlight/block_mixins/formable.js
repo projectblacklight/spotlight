430b7cf393495088b2ca68e0a16c75d3d5a0909f
Chris Beer <cabeer@stanford.edu>
2019-05-21 15:15:02 -0700

    Also verify that there's even text to bother loading

--- app/assets/javascripts/spotlight/block_mixins/formable.js
+++ app/assets/javascripts/spotlight/block_mixins/formable.js
@@ -4,18 +4,18 @@
     preload: true,
 
     initializeFormable: function() {
-      
+
       if (_.isUndefined(this['afterLoadData'])) {
         this['afterLoadData'] = function(data) { };
       }
     },
-    
+
     formId: function(id) {
       return this.blockID + "_" + id;
     },
 
     _serializeData: function() {
-      
+
       var data = $(":input,textarea,select", this.inner).not(':input:radio').serializeJSON();
 
       $(':input:radio:checked', this.inner).each(function(index, input) {
@@ -30,7 +30,7 @@
       if (this.hasTextBlock()) {
         data.text = this.getTextBlockHTML();
         data.format = 'html';
-        if (data.text.length > 0 && this.options.convertToMarkdown) {
+        if (data.text && data.text.length > 0 && this.options.convertToMarkdown) {
           data.text = stToMarkdown(data.text, this.type);
           data.format = 'markdown';
         }
@@ -38,27 +38,29 @@
 
       return data;
     },
-    
+
     loadData: function(data){
-      if (data.text.length > 0 && this.options.convertFromMarkdown && data.format !== "html") {
-        this.setTextBlockHTML(SirTrevor.toHTML(data.text, this.type));
-      } else {
-        this.setTextBlockHTML(data.text);
+      if (this.hasTextBlock()) {
+        if (data.text && data.text.length > 0 && this.options.convertFromMarkdown && data.format !== "html") {
+          this.setTextBlockHTML(SirTrevor.toHTML(data.text, this.type));
+        } else {
+          this.setTextBlockHTML(data.text);
+        }
       }
       this.loadFormDataByKey(data);
       this.afterLoadData(data);
     },
-    
+
     loadFormDataByKey: function(data) {
       $(':input', this.inner).not('button,:input[type=hidden]').each(function(index, input) {
         var key = $(input).data('key') || input.getAttribute('name');
 
         if (key) {
-        
+
           if (key.match("\\[\\]$")) {
             key = key.replace("[]", "");
           }
-          
+
           // by wrapping it in an array, this'll "just work" for radio and checkbox fields too
           var input_data = data[key];
 
@@ -70,7 +72,7 @@
       });
     },
   },
-  
+
 
   SirTrevor.Block.prototype.availableMixins.push("formable");
 })(jQuery);
