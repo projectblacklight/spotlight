6e779268bc04927fe6de69da0836d4ca816ec60f
Chris Beer <cabeer@stanford.edu>
2019-08-05 15:44:40 -0700

    Update rubocop with rubocop-rspec todos

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -5,9 +5,9 @@ describe Translation, type: :model do
 
   describe 'is unique by key, locale, and exhibit' do
     it 'fails validation' do
-      Translation.create(exhibit_id: exhibit.id, key: 'abc', locale: 'fr', value: 'yo')
+      described_class.create(exhibit_id: exhibit.id, key: 'abc', locale: 'fr', value: 'yo')
       expect do
-        Translation.create(exhibit_id: exhibit.id, key: 'abc', locale: 'fr', value: 'lo')
+        described_class.create(exhibit_id: exhibit.id, key: 'abc', locale: 'fr', value: 'lo')
       end.to raise_error(ActiveRecord::RecordNotUnique)
     end
   end
