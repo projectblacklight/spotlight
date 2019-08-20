morristech/spotlight-2

Make a little room for plugging in different page editing frameworks

app/helpers/spotlight/pages_helper.rb
@@ -6,6 +6,16 @@ module Spotlight
   module PagesHelper
     include Spotlight::RenderingHelper
 
+    def content_editor_class(page)
+      page_content = page.content_type
+
+      if page_content == 'sir_trevor'
+        'js-st-instance'
+      else
+        "js-#{page_content.dasherize}-instance"
+      end
+    end
+
     ##
     # Override the default #sir_trevor_markdown so we can use
     # a more complete markdown rendered

lib/spotlight/engine.rb
@@ -238,6 +238,7 @@ module Spotlight
 
     config.exhibit_themes = ['default']
 
+    config.default_page_content_type = 'sir_trevor'
     config.sir_trevor_widgets = %w(
       Heading Text List Quote Iframe Video Oembed Rule UploadedItems Browse LinkToSearch
       FeaturedPages SolrDocuments SolrDocumentsCarousel SolrDocumentsEmbed

projectblacklight/spotlight

app/helpers/spotlight/pages_helper.rb
@@ -6,6 +6,16 @@ module Spotlight
   module PagesHelper
     include Spotlight::RenderingHelper
 
+    def content_editor_class(page)
+      page_content = page.content_type
+
+      if page_content == 'SirTrevor'
+        'js-st-instance'
+      else
+        "js-#{page_content.parameterize}-instance"
+      end
+    end
+
     ##
     # Override the default #sir_trevor_markdown so we can use
     # a more complete markdown rendered

lib/spotlight/engine.rb
@@ -244,6 +244,7 @@ module Spotlight
 
     config.exhibit_themes = ['default']
 
+    config.default_page_content_type = 'SirTrevor'
     config.sir_trevor_widgets = %w(
       Heading Text List Quote Iframe Video Oembed Rule UploadedItems Browse LinkToSearch
       FeaturedPages SolrDocuments SolrDocumentsCarousel SolrDocumentsEmbed