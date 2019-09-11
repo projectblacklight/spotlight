cceaed642e0348e2198968fd53f4e984a19644a6
Chris Beer <cabeer@stanford.edu>
2019-09-11 13:48:48 -0700

    Add some configuration to allow uploaded field mappings to provide processing directives

--- spec/lib/spotlight/upload_field_config_spec.rb
+++ spec/lib/spotlight/upload_field_config_spec.rb
@@ -21,4 +21,20 @@ RSpec.describe Spotlight::UploadFieldConfig do
       expect(described_class.new(field_name: 'something').solr_field).to eq 'something'
     end
   end
+
+  describe '#solr_fields' do
+    it 'is backwards compatible with the old way of configuring fields' do
+      expect(described_class.new(field_name: 'something').solr_fields).to eq ['something']
+    end
+  end
+
+  describe '#data_to_solr' do
+    it 'returns a hash of the various solr fields mapped to the provided value' do
+      expect(described_class.new(field_name: 'something', solr_fields: %w[a b]).data_to_solr('value')).to eq 'a' => 'value', 'b' => 'value'
+    end
+
+    it 'supports configuring a lambda to pre-process data for a field' do
+      expect(described_class.new(field_name: 'something', solr_fields: [int_field: ->(value) { value.to_i }]).data_to_solr('123value')).to eq int_field: 123
+    end
+  end
 end
