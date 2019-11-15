679c94e7f82062a2974e47d550af7a8f75781098
Chris Beer <cabeer@stanford.edu>
2019-11-15 08:25:59 -0800

    Update browse category sort and per page partial from upstream

--- spec/views/spotlight/browse/_sort_and_per_page.html.erb_spec.rb
+++ spec/views/spotlight/browse/_sort_and_per_page.html.erb_spec.rb
@@ -1,23 +1,12 @@
 # frozen_string_literal: true
 
 describe 'spotlight/browse/_sort_and_per_page', type: :view do
-  let :blacklight_config do
-    Blacklight::Configuration.new
-  end
-
   before do
-    allow(view).to receive_messages(blacklight_config: blacklight_config)
+    allow(view).to receive_messages(render_results_collection_tools: 'collection tools')
   end
 
   it 'renders the pagination, sort, per page and view type controls' do
-    stub_template '_paginate_compact.html.erb' => "paginate_compact\n"
-    stub_template '_sort_widget.html.erb' => "sort_widget\n"
-    stub_template '_per_page_widget.html.erb' => "per_page_widget\n"
-    stub_template '_view_type_group.html.erb' => "view_type_group\n"
     render
-    expect(rendered).to_not have_content 'paginate_compact'
-    expect(rendered).to have_content 'sort_widget'
-    expect(rendered).to have_content 'per_page_widget'
-    expect(rendered).to have_content 'view_type_group'
+    expect(rendered).to have_content 'collection tools'
   end
 end
