400264f12d98a10ad385786af738b109764a9461
Jack Reed <phillipjreed@gmail.com>
2019-11-22 17:24:41 -0700

    Fixes an issue where a service account assumed only one Google Analytics profile
    A service account could have multiple profiles associated with it.
    
    Fixes https://github.com/sul-dlss/dlme/issues/522

--- spec/models/spotlight/analytics/ga_spec.rb
+++ spec/models/spotlight/analytics/ga_spec.rb
@@ -4,4 +4,19 @@ describe Spotlight::Analytics::Ga do
   it 'does not be enabled without configuration' do
     expect(described_class.enabled?).to be_falsey
   end
+
+  describe '.site' do
+    it 'selects the correct profile based on the web property id' do
+      allow(Spotlight::Engine.config).to receive_messages(ga_web_property_id: 'bar')
+      allow(described_class).to receive_message_chain(
+        :user,
+        :accounts,
+        :first,
+        profiles: [
+          double('profile1', web_property_id: 'foo'), double('profile2', web_property_id: 'bar')
+        ]
+      )
+      expect(described_class.site.web_property_id).to eq 'bar'
+    end
+  end
 end
