aa9008417a8b37512adb7a50e1594e81022e4d8e
Chris Beer <cabeer@stanford.edu>
2019-12-06 11:25:31 -0800

    Add Spotlight::ValidityChecker for checking if a job (say, indexing) still needs to be run to avoid doing duplicative work

--- spec/jobs/spotlight/reindex_job_spec.rb
+++ spec/jobs/spotlight/reindex_job_spec.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 describe Spotlight::ReindexJob do
+  include ActiveJob::TestHelper
+
   let(:exhibit) { FactoryBot.create(:exhibit) }
   let(:resource) { FactoryBot.create(:resource) }
   let(:user) { FactoryBot.create(:user) }
   let(:log_entry) { Spotlight::ReindexingLogEntry.create(exhibit: exhibit, user: user) }
 
   before do
+    ActiveJob::Base.queue_adapter = :test
     allow_any_instance_of(Spotlight::Resource).to receive(:reindex)
   end
 
@@ -67,6 +70,35 @@ describe Spotlight::ReindexJob do
 
     it 'attempts to reindex every resource in the exhibit' do
       expect(resource).to receive(:reindex)
+      subject.perform_now
+    end
+  end
+
+  describe 'validity' do
+    subject { described_class.new(resource, nil, 'xyz') }
+
+    let(:mock_checker) { instance_double(Spotlight::ValidityChecker) }
+
+    before do
+      allow(described_class).to receive(:validity_checker).and_return(mock_checker)
+      allow(mock_checker).to receive(:mint).with(resource).and_return('xyz')
+    end
+
+    it 'mints a new validity token' do
+      expect { described_class.perform_later(resource) }.to have_enqueued_job(described_class).with(resource, nil, 'xyz')
+    end
+
+    it 'does nothing if the token is no longer valid' do
+      allow(mock_checker).to receive(:check).with(resource, 'xyz').and_return(false)
+      expect(resource).not_to receive(:reindex)
+
+      subject.perform_now
+    end
+
+    it 'indexes the resource if the token is valid' do
+      allow(mock_checker).to receive(:check).with(resource, 'xyz').and_return(true)
+      expect(resource).to receive(:reindex)
+
       subject.perform_now
     end
   end
