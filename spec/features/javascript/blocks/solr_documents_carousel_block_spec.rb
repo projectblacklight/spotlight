92c69e25ad4dc63533ee0461efd77ca8842e23d8
Jessie Keck <jessie.keck@gmail.com>
2019-12-10 07:19:31 -0800

    Update references to solr_documents_block in the carousel block to solr_documents_carousel_block

---
+++ spec/features/javascript/blocks/solr_documents_carousel_block_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe 'Solr Documents Carousel Block', type: :feature, js: true do
+  let(:exhibit) { FactoryBot.create(:exhibit) }
+  let(:exhibit_curator) { FactoryBot.create(:exhibit_curator, exhibit: exhibit) }
+
+  let!(:feature_page) { FactoryBot.create(:feature_page, exhibit: exhibit) }
+
+  before do
+    login_as exhibit_curator
+
+    visit spotlight.edit_exhibit_feature_page_path(exhibit, feature_page)
+    add_widget 'solr_documents_carousel'
+  end
+
+  it 'allows a curator to select a caption to display' do
+    fill_in_typeahead_field with: 'dq287tq6352'
+
+    check 'Primary caption'
+    select 'Title', from: 'primary-caption-field'
+
+    save_page
+
+    within '.carousel-block' do
+      expect(page).to have_css('.carousel-item', count: 1)
+      expect(page).to have_css('.carousel-caption .primary', text: "L'AMERIQUE")
+    end
+  end
+end
