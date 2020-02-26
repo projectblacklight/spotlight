4820ec00b03c502ea7a766e3381e725c4af71b91
Jack Reed <phillipjreed@gmail.com>
2020-02-26 16:05:08 -0700

    Switch logic to preference pages as the first used breadcrumb (since a search session can exist)

--- app/controllers/spotlight/catalog_controller.rb
+++ app/controllers/spotlight/catalog_controller.rb
@@ -206,11 +206,11 @@ module Spotlight
 
     # rubocop:disable Metrics/AbcSize
     def add_document_breadcrumbs(document)
-      if current_browse_category
+      if current_page_context && current_page_context.title.present? && !current_page_context.is_a?(Spotlight::HomePage)
+        add_breadcrumb current_page_context.title, [current_page_context.exhibit, current_page_context]
+      elsif current_browse_category
         add_breadcrumb current_browse_category.exhibit.main_navigations.browse.label_or_default, exhibit_browse_index_path(current_browse_category.exhibit)
         add_breadcrumb current_browse_category.title, exhibit_browse_path(current_browse_category.exhibit, current_browse_category)
-      elsif current_page_context && current_page_context.title.present? && !current_page_context.is_a?(Spotlight::HomePage)
-        add_breadcrumb current_page_context.title, [current_page_context.exhibit, current_page_context]
       elsif current_search_session
         add_breadcrumb t(:'spotlight.catalog.breadcrumb.index'), search_action_url(current_search_session.query_params)
       end
