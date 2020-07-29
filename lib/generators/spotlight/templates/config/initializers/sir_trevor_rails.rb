ee1354e4a4ea6e14407afe15c806a48593a47fed
Jessie Keck <jessie.keck@gmail.com>
2020-07-29 16:01:02 -0700

    Define SirTrevor block types in Spotlight's engine config, and consume that config in the sir_trevor_rails initializer.

--- lib/generators/spotlight/templates/config/initializers/sir_trevor_rails.rb
+++ lib/generators/spotlight/templates/config/initializers/sir_trevor_rails.rb
@@ -4,25 +4,7 @@
 # https://github.com/madebymany/sir-trevor-rails#upgrade-guide-to-v060
 class SirTrevorRails::Block
   def self.custom_block_types
-    %w[
-      Browse
-      FeaturedPages
-      Heading
-      Iframe
-      LinkToSearch
-      List
-      Oembed
-      Quote
-      Rule
-      SearchResults
-      SolrDocuments
-      SolrDocumentsCarousel
-      SolrDocumentsEmbed
-      SolrDocumentsFeatures
-      SolrDocumentsGrid
-      Text
-      UploadedItems
-      Video
-    ]
+    # You can define your custom block types directly here or in your engine config.
+    Spotlight::Engine.config.sir_trevor_widgets
   end
 end
