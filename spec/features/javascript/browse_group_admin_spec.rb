f7eb21060115e66048041391bd15888c92e47c0d
Jack Reed <phillipjreed@gmail.com>
2021-01-21 16:35:56 -0700

    Add the ability to edit browse group title in place

--- spec/features/javascript/browse_group_admin_spec.rb
+++ spec/features/javascript/browse_group_admin_spec.rb
@@ -3,6 +3,13 @@
 describe 'Browse Group Adminstration', js: true do
   let(:exhibit) { FactoryBot.create(:exhibit) }
   let(:exhibit_curator) { FactoryBot.create(:exhibit_curator, exhibit: exhibit) }
+  let!(:group1) do
+    FactoryBot.create(
+      :group,
+      title: 'Good group 1',
+      exhibit: exhibit
+    )
+  end
 
   before { login_as exhibit_curator }
 
@@ -14,5 +21,25 @@ describe 'Browse Group Adminstration', js: true do
     expect(page).to have_content 'The group was created.'
     expect(page).to have_css('li.dd-item')
     expect(page).to have_css('h4', text: 'My New Group')
+  end
+
+  it 'updates the page titles' do
+    visit spotlight.exhibit_searches_path(exhibit, anchor: 'browse-groups')
+
+    within("[data-id='#{group1.id}']") do
+      within('h4') do
+        expect(page).to have_content('Good group 1')
+        expect(page).to have_css('.title-field', visible: false)
+        click_link('Good group 1')
+        expect(page).to have_css('.title-field', visible: true)
+        find('.title-field').set('New good group 1')
+      end
+    end
+    click_button('Save changes')
+    within("[data-id='#{group1.id}']") do
+      within('h4') do
+        expect(page).to have_content('New good group 1')
+      end
+    end
   end
 end
