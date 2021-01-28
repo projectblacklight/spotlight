06e627a0b031d799545d0238c578fa98360a0f4e
Chris Beer <cabeer@stanford.edu>
2021-01-28 14:32:59 -0800

    Extract Spotlight::LimitConcurrency concern

--- spec/jobs/spotlight/reindex_job_spec.rb
+++ spec/jobs/spotlight/reindex_job_spec.rb
@@ -75,28 +75,28 @@ describe Spotlight::ReindexJob do
   end
 
   describe 'validity' do
-    subject { described_class.new(resource, nil, 'xyz') }
+    subject { described_class.new(resource, 'validity_token' => 'xyz') }
 
     let(:mock_checker) { instance_double(Spotlight::ValidityChecker) }
 
     before do
       allow(described_class).to receive(:validity_checker).and_return(mock_checker)
-      allow(mock_checker).to receive(:mint).with(resource).and_return('xyz')
+      allow(mock_checker).to receive(:mint).with(anything).and_return('xyz')
     end
 
     it 'mints a new validity token' do
-      expect { described_class.perform_later(resource) }.to have_enqueued_job(described_class).with(resource, nil, 'xyz')
+      expect { described_class.perform_later(resource) }.to have_enqueued_job(described_class).with(resource, 'validity_token' => 'xyz')
     end
 
     it 'does nothing if the token is no longer valid' do
-      allow(mock_checker).to receive(:check).with(resource, 'xyz').and_return(false)
+      allow(mock_checker).to receive(:check).with(subject, validity_token: 'xyz').and_return(false)
       expect(resource).not_to receive(:reindex)
 
       subject.perform_now
     end
 
     it 'indexes the resource if the token is valid' do
-      allow(mock_checker).to receive(:check).with(resource, 'xyz').and_return(true)
+      allow(mock_checker).to receive(:check).with(subject, validity_token: 'xyz').and_return(true)
       expect(resource).to receive(:reindex)
 
       subject.perform_now
