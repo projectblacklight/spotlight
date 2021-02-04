7576b8d01617d59d2e4a8e3cf6ab4f8ef32f97a3
Chris Beer <cabeer@stanford.edu>
2021-02-04 07:40:45 -0800

    Add some tests for ReindexExhibitJob

---
+++ spec/jobs/spotlight/reindex_exhibit_job_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+describe Spotlight::ReindexExhibitJob do
+  let(:exhibit) { FactoryBot.create(:exhibit) }
+
+  before do
+    FactoryBot.create_list(:resource, 10, exhibit: exhibit)
+    allow(Spotlight::ReindexJob).to receive(:perform_now)
+    allow(Spotlight::ReindexJob).to receive(:perform_later)
+  end
+
+  context 'with a single batch' do
+    it 'runs the index job inline' do
+      described_class.perform_now(exhibit, batch_count: 1)
+
+      expect(Spotlight::ReindexJob).to have_received(:perform_now).once.with(exhibit, anything)
+    end
+  end
+
+  context 'with a fixed batch count' do
+    it 'enqueues that number of batches' do
+      described_class.perform_now(exhibit, batch_count: 2)
+
+      expect(Spotlight::ReindexJob).to have_received(:perform_later).twice.with(exhibit, hash_including(:start, :finish))
+    end
+  end
+
+  context 'with a dynamically generated batch count' do
+    it 'enqueues the right number of batches' do
+      described_class.perform_now(exhibit, batch_count: nil, batch_size: 2)
+
+      expect(Spotlight::ReindexJob).to have_received(:perform_later).exactly(5).times.with(exhibit, hash_including(:start, :finish))
+    end
+  end
+
+  context 'with a dynamically generated batch size' do
+    it 'figues out that number of batches' do
+      described_class.perform_now(exhibit, batch_count: nil, batch_size: nil)
+
+      expect(Spotlight::ReindexJob).to have_received(:perform_later).exactly(3).times.with(exhibit, hash_including(:start, :finish))
+    end
+  end
+end
