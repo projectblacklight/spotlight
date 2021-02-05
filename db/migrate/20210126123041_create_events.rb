c9ff4e13054eb74cdcc30ead88c14c6320ca857f
Chris Beer <cabeer@stanford.edu>
2021-02-05 10:04:09 -0800

    Fix event exhibits reference migration

--- db/migrate/20210126123041_create_events.rb
+++ db/migrate/20210126123041_create_events.rb
@@ -3,7 +3,7 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :spotlight_events do |t|
-      t.references :exhibit, null: false, polymorphic: true
+      t.references :exhibit, null: true
       t.references :resource, null: false, polymorphic: true, index: true
       t.string :type
       t.string :collation_key
