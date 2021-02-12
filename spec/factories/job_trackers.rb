6aa04b6fb2b4fadafa646c06be9f89e51f7351f9
Chris Beer <cabeer@stanford.edu>
2021-02-12 07:45:02 -0800

    Add placeholder data to job_tracker factory to appease the linter

--- spec/factories/job_trackers.rb
+++ spec/factories/job_trackers.rb
@@ -3,6 +3,8 @@
 FactoryBot.define do
   factory :job_tracker, class: 'Spotlight::JobTracker' do
     user
+    on factory: :exhibit
+    resource factory: :exhibit
     created_at { Time.zone.parse('2017-01-05 23:00:00') }
     updated_at { Time.zone.parse('2017-01-05 23:05:00') }
   end
