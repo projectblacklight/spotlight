e9b2d82d257473de8542c55a910dbda474b9d51b
E. Lynette Rayle <elr37@cornell.edu>
2021-02-23 17:15:55 -0500

    generated initializer list of sir treavor widgets should match configured list

--- lib/generators/spotlight/templates/config/initializers/spotlight_initializer.rb
+++ lib/generators/spotlight/templates/config/initializers/spotlight_initializer.rb
@@ -73,7 +73,7 @@
 # These are set by default by Spotlight's configuration,
 # but you can customize them here, or in the SirTrevorRails::Block#custom_block_types method
 # Spotlight::Engine.config.sir_trevor_widgets = %w(
-#   Heading Text List Quote Iframe Video Oembed Rule UploadedItems Browse BrowseGroupCategories
+#   Heading Text List Quote Iframe Video Oembed Rule UploadedItems Browse BrowseGroupCategories LinkToSearch
 #   FeaturedPages SolrDocuments SolrDocumentsCarousel SolrDocumentsEmbed
 #   SolrDocumentsFeatures SolrDocumentsGrid SearchResults
 # )
