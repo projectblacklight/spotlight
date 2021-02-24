d850e537365b61715d026db4548435b3afd559aa
Chris Beer <chris@cbeer.info>
2021-02-24 07:47:11 -0800

    Fix cursor loop logic

--- app/jobs/spotlight/change_visibility_job.rb
+++ app/jobs/spotlight/change_visibility_job.rb
@@ -20,10 +20,11 @@ module Spotlight
     def each_document(solr_params, exhibit, &block)
       return to_enum(:each_document, solr_params, exhibit) unless block_given?
 
-      cursor_mark = '*'
-      response = {}
+      cursor_mark = nil
+      next_cursor_mark = '*'
 
-      while response['nextCursorMark'] != cursor_mark
+      until next_cursor_mark == cursor_mark || next_cursor_mark.nil?
+        cursor_mark = next_cursor_mark
         response = exhibit.blacklight_config.repository.search(
           solr_params.merge(
             'rows' => Spotlight::Engine.config.bulk_actions_batch_size,
@@ -36,7 +37,7 @@ module Spotlight
           block.call(document)
         end
 
-        cursor_mark = response['nextCursorMark']
+        next_cursor_mark = response['nextCursorMark']
       end
     end
     # rubocop:enable Metrics/MethodLength
