69ce632f9396bcd89b2556c4c23a76273c23fc88
Jack Reed <phillipjreed@gmail.com>
2021-02-23 18:43:06 -0700

    Use cursorMark to efficiently paginate Solr responses

--- app/jobs/spotlight/change_visibility_job.rb
+++ app/jobs/spotlight/change_visibility_job.rb
@@ -4,8 +4,8 @@ module Spotlight
   ###
   class ChangeVisibilityJob < Spotlight::ApplicationJob
     def perform(solr_params:, exhibit:, visibility:, **)
-      response = exhibit.blacklight_config.repository.search(solr_params.merge('rows' => 999_999_999))
-      response.documents.each do |document|
+      documents = retrieve_documents(solr_params, exhibit)
+      documents.each do |document|
         case visibility
         when 'public'
           document.make_public!(exhibit)
@@ -15,5 +15,26 @@ module Spotlight
         document.reindex
       end
     end
+
+    # rubocop:disable Metrics/MethodLength
+    def retrieve_documents(solr_params, exhibit)
+      cursor_mark = '*'
+      documents = []
+      loop do
+        response = exhibit.blacklight_config.repository.search(
+          solr_params.merge(
+            'rows' => Spotlight::Engine.config.bulk_actions_batch_size,
+            'cursorMark' => cursor_mark,
+            'sort' => "#{exhibit.blacklight_config.document_model.unique_key} asc"
+          )
+        )
+        documents.concat response.documents
+        break if response['nextCursorMark'] == cursor_mark
+
+        cursor_mark = response['nextCursorMark']
+      end
+      documents
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
