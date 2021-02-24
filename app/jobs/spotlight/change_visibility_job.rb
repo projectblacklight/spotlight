52e5b97063b1c6952cf7ebc8fb10994a059261d1
Jack Reed <phillipjreed@gmail.com>
2021-02-24 16:01:27 -0700

    Add job tracking to ChangeVisibilityJob

--- app/jobs/spotlight/change_visibility_job.rb
+++ app/jobs/spotlight/change_visibility_job.rb
@@ -3,7 +3,14 @@
 module Spotlight
   ###
   class ChangeVisibilityJob < Spotlight::ApplicationJob
+    include Spotlight::JobTracking
+
+    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
     def perform(solr_params:, exhibit:, visibility:, **)
+      job_tracker.update(status: 'in_progress')
+
+      @errors = 0
+
       each_document(solr_params, exhibit) do |document|
         case visibility
         when 'public'
@@ -12,8 +19,24 @@ module Spotlight
           document.make_private!(exhibit)
         end
         document.reindex(update_params: {})
+        progress&.increment
+      rescue StandardError => e
+        job_tracker.append_log_entry(type: :error, exhibit: exhibit, message: e.to_s)
+        @errors += 1
       end
       exhibit.blacklight_config.repository.connection.commit
+      job_tracker.append_log_entry(type: :info, exhibit: exhibit, message: "#{progress.progress} of #{progress.total} (#{@errors} errors)")
+    ensure
+      job_tracker.update(status: @errors.zero? ? 'completed' : 'failed', data: { progress: progress.progress, total: progress.total })
+    end
+    # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+
+    def job_tracking_resource
+      arguments.last[:exhibit]
+    end
+
+    def reports_on_resource
+      arguments.last[:exhibit] if arguments.last.is_a?(Hash)
     end
 
     # rubocop:disable Metrics/MethodLength
@@ -32,7 +55,7 @@ module Spotlight
             'sort' => "#{exhibit.blacklight_config.document_model.unique_key} asc"
           )
         )
-
+        progress.total = response.total
         response.documents.each do |document|
           block.call(document)
         end
