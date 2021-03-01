28266f7cf5a567da85c853a921cc8797a7fbaf0d
Chris Beer <chris@cbeer.info>
2021-03-01 13:42:38 -0800

    Spike on processing CSV uploads

---
+++ app/jobs/spotlight/process_bulk_updates_csv_job.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+module Spotlight
+  ###
+  class ProcessBulkUpdatesCsvJob < Spotlight::ApplicationJob
+    include Spotlight::JobTracking
+    with_job_tracking(resource: ->(job) { job.arguments.first })
+
+    # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+    def perform(exhibit, bulk_update)
+      errors = 0
+      header_converter = ->(header) { header } # Use raw header for columns (since they are configured)
+      csv_path = bulk_update.file.current_path
+      File.open(csv_path) do |f|
+        progress&.total = f.each_line.count
+
+        ::CSV.table(f, header_converters: header_converter).each do |row|
+          document = exhibit.blacklight_config.document_model.find(row[config.csv_id])
+          sidecar = document.sidecar(exhibit)
+
+          raise 'Unable to locate document' if document.blank?
+
+          sidecar.update(public: row[config.csv_visibility])
+
+          added_tags_cols, removed_tags_cols = row.headers.select { |k| k =~ /^#{config.csv_tags_prefix}/ }.partition do |k|
+            ActiveModel::Type::Boolean.new.cast(row[k])
+          end
+
+          added_tags = added_tags_cols.map { |x| x.sub(/#{config.csv_tags_prefix}\s/, '') }
+          removed_tags = removed_tags_cols.map { |x| x.sub(/#{config.csv_tags_prefix}\s/, '') }
+
+          all_tags = sidecar.all_tags_list
+
+          exhibit.tag(sidecar, with: all_tags - removed_tags + added_tags, on: :tags)
+
+          document.reindex(update_params: {})
+
+          progress&.increment
+        rescue StandardError => e
+          job_tracker.append_log_entry(type: :error, exhibit: exhibit, message: e.to_s)
+          errors += 1
+          mark_job_as_failed!
+        end
+
+        exhibit.blacklight_config.repository.connection.commit
+        job_tracker.append_log_entry(type: :info, exhibit: exhibit, message: "#{progress&.progress} of #{progress&.total} (#{errors} errors)")
+      end
+    end
+    # rubocop:enable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/MethodLength, Metrics/PerceivedComplexity
+
+    private
+
+    def config
+      Spotlight::Engine.config.bulk_updates
+    end
+  end
+end
