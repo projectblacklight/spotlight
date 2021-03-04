e442b4a54e534cf377f33feb735f19dada2b503d
Gary Geisler <ggeisler@gmail.com>
2021-03-03 18:04:11 -0700

    Update text and styling of bulk actions modals

--- spec/features/bulk_actions_spec.rb
+++ spec/features/bulk_actions_spec.rb
@@ -29,7 +29,7 @@ describe 'Bulk actions', type: :feature do
     click_link 'Change item visibility'
     expect(page).to have_css 'h4', text: 'Change item visibility', visible: true
     choose 'Private'
-    accept_confirm 'Are you sure?' do
+    accept_confirm 'All items in the result set will be updated. Are you sure?' do
       click_button 'Change'
     end
     expect(page).to have_css '.alert', text: 'Visibility of 1 item is being updated.'
@@ -46,7 +46,7 @@ describe 'Bulk actions', type: :feature do
       find('[data-autocomplete-fetched="true"]', visible: false)
       find('.tt-input').set('good,stuff')
     end
-    accept_confirm 'Are you sure?' do
+    accept_confirm 'All items in the result set will be updated. Are you sure?' do
       click_button 'Add'
     end
     expect(page).to have_css '.alert', text: 'Tags are being added for 1 item.'
@@ -63,7 +63,7 @@ describe 'Bulk actions', type: :feature do
       find('[data-autocomplete-fetched="true"]', visible: false)
       find('.tt-input').set('foo')
     end
-    accept_confirm 'Are you sure?' do
+    accept_confirm 'All items in the result set will be updated. Are you sure?' do
       click_button 'Remove'
     end
     expect(page).to have_css '.alert', text: 'Tags are being removed for 1 item.'
