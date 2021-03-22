f1169e19631e2c79c88648ca310375634739e405
Chris Beer <chris@cbeer.info>
2021-03-22 14:14:02 -0700

    Add a query facet for 'public' items

--- app/controllers/concerns/spotlight/catalog.rb
+++ app/controllers/concerns/spotlight/catalog.rb
@@ -14,18 +14,24 @@ module Spotlight
 
     # Adds a facet to display document visibility for the current exhibit
     # if the user is a curator
+    # rubocop:disable Metrics/MethodLength
     def add_facet_visibility_field
       return unless current_exhibit && can?(:curate, current_exhibit)
 
       blacklight_config.add_facet_field 'exhibit_visibility',
                                         label: I18n.t(:'spotlight.catalog.facets.exhibit_visibility.label'),
                                         query: {
+                                          public: {
+                                            label: I18n.t(:'spotlight.catalog.facets.exhibit_visibility.public'),
+                                            fq: "-#{blacklight_config.document_model.visibility_field(current_exhibit)}:false"
+                                          },
                                           private: {
                                             label: I18n.t(:'spotlight.catalog.facets.exhibit_visibility.private'),
                                             fq: "#{blacklight_config.document_model.visibility_field(current_exhibit)}:false"
                                           }
                                         }
     end
+    # rubocop:enable Metrics/MethodLength
 
     def render_curator_actions?
       (current_exhibit && can?(:curate, current_exhibit)) &&
