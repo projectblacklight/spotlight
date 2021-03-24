9af85932547514ae69fbc5847ec8fa0e655b1094
Chris Beer <chris@cbeer.info>
2021-03-24 11:24:32 -0700

    Wrap RIIIF configuration in `ActiveSupport::Reloader.to_prepare`

--- lib/generators/spotlight/templates/config/initializers/riiif.rb
+++ lib/generators/spotlight/templates/config/initializers/riiif.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+ActiveSupport::Reloader.to_prepare do
+  Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
 
-Riiif::Image.file_resolver = Spotlight::CarrierwaveFileResolver.new
+  # Riiif::Image.authorization_service = IIIFAuthorizationService
 
-# Riiif::Image.authorization_service = IIIFAuthorizationService
-
-# Riiif.not_found_image = 'app/assets/images/us_404.svg'
-#
-Riiif::Engine.config.cache_duration = 365.days
+  # Riiif.not_found_image = 'app/assets/images/us_404.svg'
+  #
+  Riiif::Engine.config.cache_duration = 365.days
+end
