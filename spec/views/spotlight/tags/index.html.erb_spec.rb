8aebabc1a144d10458511b33413ecc9f11d34f71
Jack Reed <phillipjreed@gmail.com>
2021-04-05 07:08:03 -0600

    Update test to reflect new edit in place behavior

--- spec/views/spotlight/tags/index.html.erb_spec.rb
+++ spec/views/spotlight/tags/index.html.erb_spec.rb
@@ -11,14 +11,17 @@ describe 'spotlight/tags/index.html.erb', type: :view do
     allow(view).to receive_messages(exhibit_tag_path: '/tags')
     allow(view).to receive(:current_exhibit).and_return(exhibit)
     allow(view).to receive(:url_to_tag_facet, &:first)
+    allow(view).to receive_messages(update_all_exhibit_tags_path: '/update_all')
+    allow(view).to receive(:exhibit_path)
   end
 
   describe 'Tags' do
     it 'is displayed' do
       render
+      puts rendered
       [tag1.tag.name, tag2.tag.name].each do |name|
-        expect(rendered).to have_css('td', text: name)
-        expect(rendered).to have_link(name, href: 't')
+        expect(rendered).to have_css('h4', text: name)
+        expect(rendered).to have_link(name, href: '#edit-in-place')
       end
     end
   end
