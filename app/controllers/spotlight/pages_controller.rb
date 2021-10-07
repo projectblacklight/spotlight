dd254537796b5c2b37761123bbbac1892d3a388d
Cory Lown <corylown@stanford.edu>
2021-10-07 17:03:01 -0400

    Rubocop fixes

--- app/controllers/spotlight/pages_controller.rb
+++ app/controllers/spotlight/pages_controller.rb
@@ -3,9 +3,6 @@
 module Spotlight
   ##
   # Base CRUD controller for pages
-  # rubocop:disable Metrics/ClassLength
-  # Disableing class length because this is a base
-  # controller that gives other controllers their behavior
   class PagesController < Spotlight::ApplicationController
     before_action :authenticate_user!, except: [:show]
     before_action :load_locale_specific_page, only: %i[destroy edit show update]
@@ -208,5 +205,4 @@ module Spotlight
       end
     end
   end
-  # rubocop:enable Metrics/ClassLength
 end
