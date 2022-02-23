eb443850fa975af16a0a7fad371db7ea130ad30c
Chris Beer <chris@cbeer.info>
2022-02-23 10:21:25 -0800

    Use the actual configured facet limit to determine when to show the 'too many values' version of the count (#2792)

--- spec/views/spotlight/search_configurations/_facet_metadata.html.erb_spec.rb
+++ spec/views/spotlight/search_configurations/_facet_metadata.html.erb_spec.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 describe 'spotlight/search_configurations/_facet_metadata', type: :view do
+  let(:field_config) { nil }
+  let(:blacklight_config) { nil }
+
   before do
-    render partial: 'spotlight/search_configurations/facet_metadata', locals: { metadata: metadata }
+    render partial: 'spotlight/search_configurations/facet_metadata', locals: {
+      blacklight_config: blacklight_config,
+      field_config: field_config,
+      metadata: metadata
+    }
   end
 
   context 'with a facet without any documents' do
@@ -27,6 +34,25 @@ describe 'spotlight/search_configurations/_facet_metadata', type: :view do
 
     it 'shows there are many unique values' do
       expect(rendered).to have_content '20+ unique values'
+    end
+  end
+
+  context 'with a facet with a configured limit' do
+    let(:metadata) { { document_count: 1, value_count: 10, terms: %w[] } }
+    let(:field_config) { Blacklight::Configuration::FacetField.new(limit: 10) }
+
+    it 'shows there are many unique values' do
+      expect(rendered).to have_content '10+ unique values'
+    end
+  end
+
+  context 'with a facet with an implicit limit' do
+    let(:blacklight_config) { CatalogController.blacklight_config }
+    let(:metadata) { { document_count: 1, value_count: 13, terms: %w[] } }
+    let(:field_config) { Blacklight::Configuration::FacetField.new(limit: true) }
+
+    it 'shows there are many unique values' do
+      expect(rendered).to have_content '10+ unique values'
     end
   end
 end
