44eecd475c05c601ac4febac8c828e6af51cc85d
Chris Beer <chris@cbeer.info>
2022-02-25 09:35:51 -0800

    Update confirmation email text; fixes #2779

--- app/mailers/spotlight/confirmation_mailer.rb
+++ app/mailers/spotlight/confirmation_mailer.rb
@@ -6,8 +6,9 @@ module Spotlight
   class ConfirmationMailer < ActionMailer::Base
     include Devise::Mailers::Helpers
 
-    def confirmation_instructions(record, token, opts)
+    def confirmation_instructions(record, token, opts, exhibit: nil)
       @token = token
+      @exhibit = exhibit
       initialize_from_record(record)
       mail headers_for(:confirmation_instructions, opts)
     end
