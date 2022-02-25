a573e2f7e74db0f20021e34a4ba23596ead3148e
Chris Beer <chris@cbeer.info>
2022-02-25 09:25:25 -0800

    Tweak log messages and comments. Fixes #2780

--- app/helpers/spotlight/main_app_helpers.rb
+++ app/helpers/spotlight/main_app_helpers.rb
@@ -40,7 +40,7 @@ module Spotlight
       if current_exhibit_theme && current_exhibit&.theme != 'default'
         stylesheet_link_tag "#{tag}_#{current_exhibit_theme}"
       else
-        Rails.logger.warn "Exhibit theme '#{current_exhibit_theme}' not in white-list of available themes: #{current_exhibit&.themes}"
+        Rails.logger.warn "Exhibit theme '#{current_exhibit_theme}' not in the list of available themes: #{current_exhibit&.themes}"
         stylesheet_link_tag(tag)
       end
     end
