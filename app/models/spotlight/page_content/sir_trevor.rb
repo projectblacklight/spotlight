005ed394fa7e5e4bd79c05d1736bc688521cb2ac
Chris Beer <chris@cbeer.info>
2022-02-28 13:22:52 -0800

    Adapt SirTrevorRails::Block to our coding standards

--- app/models/spotlight/page_content/sir_trevor.rb
+++ app/models/spotlight/page_content/sir_trevor.rb
@@ -5,18 +5,16 @@ module Spotlight
     # Sir-Trevor created content
     class SirTrevor
       def self.parse(page, attribute)
-        content = page.read_attribute(attribute)
-        content ||= [].to_json
+        blocks = page.read_attribute(attribute)
+        blocks ||= [].to_json
 
         return [] if blocks.blank?
 
-        if blocks.is_a?(String)
-          blocks = JSON.parse(blocks, symbolize_names: true)
-        end
+        blocks = JSON.parse(blocks, symbolize_names: true) if blocks.is_a?(String)
 
         if blocks.is_a?(Hash)
           blocks = blocks[:data] || blocks['data'] or
-            raise IndexError, "No block data found"
+            raise IndexError, 'No block data found'
         end
 
         blocks.map do |obj|
--- spec/models/sir_trevor_rails/block_spec.rb
+++ spec/models/sir_trevor_rails/block_spec.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 RSpec.describe SirTrevorRails::Block do
   describe '.block_class' do
-    subject { Block }
+    subject { described_class }
 
     describe 'for known type' do
       let(:klass) { subject.block_class('tweet') }
 
       it 'returns class' do
-        expect { klass == Blocks::TweetBlock }
+        expect(klass).to eq SirTrevorRails::Blocks::TweetBlock
       end
 
       it 'returned class is the one defined in file' do
-        expect { klass.method_defined? :at_name }
+        expect(klass.method_defined?(:at_name)).to be true
       end
     end
 
@@ -20,51 +22,51 @@ RSpec.describe SirTrevorRails::Block do
       let(:klass) { subject.block_class('unknown') }
 
       it 'defines and returns class' do
-        expect { klass == Blocks::UnknownBlock }
+        expect(klass).to eq SirTrevorRails::Blocks::UnknownBlock
       end
 
       it 'defines class that inherits from Block' do
-        expect { klass.superclass == Block }
+        expect(klass.superclass).to eq described_class
       end
     end
   end
 
   describe '.from_hash' do
     let(:source_hash) { { type: 'test', data: {} } }
-    let(:block) { Block.from_hash(source_hash, nil) }
+    let(:block) { described_class.from_hash(source_hash, nil) }
 
     it 'initializes new block based on type field' do
-      expect { block.is_a? Blocks::TestBlock }
+      expect(block).to be_a_kind_of SirTrevorRails::Blocks::TestBlock
     end
 
     it 'creates accessors for all fields defined in data field' do
       source_hash[:data].merge!(width: '100')
 
-      expect { block.width == '100' }
+      expect(block.width).to eq '100'
     end
   end
 
   describe 'JSON representation' do
     let(:empty_source_hash) { { type: 'test', data: {} } }
-    let(:empty_block) { Block.from_hash(empty_source_hash, nil) }
+    let(:empty_block) { described_class.from_hash(empty_source_hash, nil) }
     let(:data_source_hash) { { type: 'test', data: { one: 2, three: 4 } } }
-    let(:data_block) { Block.from_hash(data_source_hash, nil) }
+    let(:data_block) { described_class.from_hash(data_source_hash, nil) }
 
     it 'returns source hash when #as_json is called' do
-      expect { empty_block.as_json == empty_source_hash }
+      expect(empty_block.as_json).to eq empty_source_hash
     end
 
     it 'serializes block data' do
-      expect { data_block.as_json == data_source_hash }
+      expect(data_block.as_json).to eq data_source_hash
     end
   end
 
   describe 'partial view lookup' do
     let(:source_hash) { { type: 'test', data: {} } }
-    let(:block) { Block.from_hash(source_hash, nil) }
+    let(:block) { described_class.from_hash(source_hash, nil) }
 
     it 'uses block type to find view paritals' do
-      expect { block.to_partial_path == 'sir_trevor/blocks/test_block' }
+      expect(block.to_partial_path).to eq 'sir_trevor/blocks/test_block'
     end
   end
 end
