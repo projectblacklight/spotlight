43d06e9f075a860ff6cd38d0a8ad68cbb13b4c9d
Matt Critchlow <mcritchlow@ucsd.edu>
2022-04-14 13:48:51 -0700

    Render Analytics link in sidebar when enabled
    If Analytics is not enabled, we should not show an Analytics link in the
    sidebar for an Exhibit.

---
+++ spec/views/spotlight/shared/_exhibit_sidebar.html.erb_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe 'spotlight/shared/_exhibit_sidebar', type: :view do
+  let(:current_exhibit) { FactoryBot.create(:exhibit) }
+
+  before do
+    allow(view).to receive_messages(current_exhibit: current_exhibit, exhibit_root_path: '/some/path')
+  end
+
+  context 'with a configured analytics integration' do
+    before do
+      allow(current_exhibit).to receive(:analytics_provider).and_return(double(Spotlight::Analytics::Ga, enabled?: true))
+    end
+
+    it 'has an analytics link in the sidebar' do
+      render
+
+      expect(rendered).to have_link 'Analytics'
+    end
+  end
+
+  context 'without a configured analytics integration' do
+    it 'does not have an analytics link in the sidebar' do
+      render
+
+      expect(rendered).not_to have_link 'Analytics'
+    end
+  end
+end
