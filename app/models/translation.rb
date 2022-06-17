9501f1045128863e627f907cd56624a24af1eb7e
Chris Beer <chris@cbeer.info>
2022-06-17 10:18:19 -0700

    Work around upstream i18n-active-record behavior that's incompatible with ruby 2.7

---
+++ app/models/translation.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+unless defined?(Translation)
+  Translation = I18n::Backend::ActiveRecord::Translation
+  Translation.include Spotlight::CustomTranslationExtension
+
+  # Work-around for https://github.com/svenfuchs/i18n-active_record/pull/133
+  if Translation.respond_to?(:to_hash)
+    class << Translation
+      alias to_h to_hash
+      remove_method :to_hash
+    end
+
+    I18n::Backend::ActiveRecord.define_method(:init_translations) do
+      @translations = Translation.to_h
+    end
+  end
+end
--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -11,4 +11,16 @@ describe Translation, type: :model do
       end.to raise_error(ActiveRecord::RecordNotUnique)
     end
   end
+
+  describe '.to_h' do
+    it 'dumps the translations' do
+      expect(described_class.to_h).to be_a(Hash)
+    end
+  end
+
+  describe '.to_hash' do
+    it 'is not defined' do
+      expect(described_class).not_to respond_to(:to_hash)
+    end
+  end
 end
