projectblacklight/spotlight

commit 83a6c47b959b9d05dbd3a437736a077d5630355f

Remove hard gem dependencies on application dependencies

Dockerfile
@@ -41,7 +41,7 @@ RUN bundle install --jobs "$(nproc)"
 
 RUN mkdir -p /spotlight/app
 WORKDIR /spotlight/app
-RUN SKIP_TRANSLATION=yes rails _${RAILS_VERSION}_ new . --force --template=../engine/template.rb
+RUN SKIP_TRANSLATION=yes rails _${RAILS_VERSION}_ new . -d postgresql -j webpack --force --template=../engine/template.rb
 
 RUN SKIP_TRANSLATION=yes DB_ADAPTER=nulldb DATABASE_URL='postgresql://fake' bundle exec rake assets:precompile
 

blacklight-spotlight.gemspec
@@ -43,27 +43,23 @@ these collections.)
   s.add_dependency 'iiif-presentation'
   s.add_dependency 'leaflet-rails'
   s.add_dependency 'legato'
-  s.add_dependency 'mimemagic', '0.3.8'
   s.add_dependency 'mini_magick'
   s.add_dependency 'nokogiri'
   s.add_dependency 'oauth2'
   s.add_dependency 'openseadragon'
   s.add_dependency 'ostruct', '!= 0.3.0', '!= 0.3.1', '!= 0.3.2'
   s.add_dependency 'paper_trail', '>= 11.0', '< 13'
-  s.add_dependency 'pg'
   s.add_dependency 'rails', '>= 6.1', '< 8'
   s.add_dependency 'redcarpet', '>= 2.0.1', '< 4'
   s.add_dependency 'riiif', '~> 2.0'
   s.add_dependency 'roar', '~> 1.1'
   s.add_dependency 'roar-rails'
-  s.add_dependency 'sidekiq'
   s.add_dependency 'signet'
   s.add_dependency 'sprockets', '>= 3'
   s.add_dependency 'sprockets-es6'
   s.add_dependency 'thor'
   s.add_dependency 'tophat'
   s.add_dependency 'underscore-rails', '~> 1.6'
-  s.add_dependency 'webpacker'
 
   s.add_development_dependency 'capybara', '~> 3.31'
   s.add_development_dependency 'engine_cart', '~> 2.0'

lib/generators/spotlight/install_generator.rb
@@ -177,14 +177,6 @@ module Spotlight
       copy_file 'config/initializers/translation.rb'
     end
 
-    def configure_queue
-      insert_into_file 'config/application.rb', after: "< Rails::Application\n" do
-        <<-EOF
-        config.active_job.queue_adapter = ENV["RAILS_QUEUE"]&.to_sym || :sidekiq
-        EOF
-      end
-    end
-
     def configure_logging
       insert_into_file 'config/application.rb', after: "< Rails::Application\n" do
         <<-EOF

template.rb
@@ -10,6 +10,7 @@ spotlight_options = ENV.fetch('SPOTLIGHT_INSTALL_OPTIONS', DEFAULT_SPOTLIGHT_OPT
 # Add gem dependencies to the application
 gem 'blacklight', ' ~> 7.0'
 gem 'blacklight-spotlight', ENV['SPOTLIGHT_GEM'] ? { path: ENV['SPOTLIGHT_GEM'] } : { github: 'projectblacklight/spotlight' }
+gem 'sidekiq'
 
 Bundler.with_clean_env do
   run 'bundle install'
@@ -26,4 +27,10 @@ rake 'spotlight:install:migrations'
 if !options['quiet'] && yes?('Would you like to create an initial administrator?')
   rake 'db:migrate' # we only need to run the migrations if we are creating an admin user
   rake 'spotlight:initialize'
+end
+
+insert_into_file 'config/application.rb', after: "< Rails::Application\n" do
+  <<-CONFIG
+  config.active_job.queue_adapter = ENV["RAILS_QUEUE"]&.to_sym || :sidekiq
+  CONFIG
 end

commit e6069e551a3ab1f0224ec6c453de5860963e94b1

fix docker build
broken in https://github.com/projectblacklight/spotlight/commit/83a6c47b959b9d05dbd3a437736a077d5630355f

Dockerfile
@@ -41,8 +41,9 @@ RUN bundle install --jobs "$(nproc)"
 
 RUN mkdir -p /spotlight/app
 WORKDIR /spotlight/app
-RUN SKIP_TRANSLATION=yes rails _${RAILS_VERSION}_ new . -d postgresql -j webpack --force --template=../engine/template.rb
 
+RUN SKIP_TRANSLATION=yes rails _${RAILS_VERSION}_ new . -j webpack --force --template=../engine/template.rb
+RUN bundle add pg
 RUN SKIP_TRANSLATION=yes DB_ADAPTER=nulldb DATABASE_URL='postgresql://fake' bundle exec rake assets:precompile
 
 ENTRYPOINT ["/sbin/tini", "--", "/spotlight/engine/bin/docker-entrypoint.sh"]