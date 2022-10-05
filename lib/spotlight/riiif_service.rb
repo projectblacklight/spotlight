a6ceb8c9024762da0c07815f32971d84aae1240e
Alexandra Dunn <dunn.alex@gmail.com>
2022-10-05 09:42:47 -0700

    run rubocop, remove now-invalid cop definition

--- lib/spotlight/riiif_service.rb
+++ lib/spotlight/riiif_service.rb
@@ -1,34 +1,36 @@
 # frozen_string_literal: true
 
-module Spotlight::RiiifService
-  # @param [Spotlight::FeaturedImage] image
-  # @return [String]
-  def self.thumbnail_path(image)
-    Riiif::Engine.routes.url_helpers.image_path(image, size: '!400,400')
-  end
+module Spotlight
+  module RiiifService
+    # @param [Spotlight::FeaturedImage] image
+    # @return [String]
+    def self.thumbnail_path(image)
+      Riiif::Engine.routes.url_helpers.image_path(image, size: '!400,400')
+    end
 
-  # @param [Spotlight::FeaturedImage] image
-  # @return [String]
-  def self.info_url(image)
-    Riiif::Engine.routes.url_helpers.info_url(image)
-  end
+    # @param [Spotlight::FeaturedImage] image
+    # @return [String]
+    def self.info_url(image)
+      Riiif::Engine.routes.url_helpers.info_url(image)
+    end
 
-  # @param [Spotlight::FeaturedImage] image
-  # @return [String]
-  def self.info_path(image)
-    Riiif::Engine.routes.url_helpers.info_path(image)
-  end
+    # @param [Spotlight::FeaturedImage] image
+    # @return [String]
+    def self.info_path(image)
+      Riiif::Engine.routes.url_helpers.info_path(image)
+    end
 
-  # @param [Spotlight::Exhibit] exhibit
-  # @param [Spotlight::FeaturedImage] image
-  # @return [String]
-  def self.manifest_path(exhibit, image)
-    Spotlight::Engine.routes.url_helpers.manifest_exhibit_solr_document_path(exhibit, "#{exhibit.id}-#{image.id}")
-  end
+    # @param [Spotlight::Exhibit] exhibit
+    # @param [Spotlight::FeaturedImage] image
+    # @return [String]
+    def self.manifest_path(exhibit, image)
+      Spotlight::Engine.routes.url_helpers.manifest_exhibit_solr_document_path(exhibit, "#{exhibit.id}-#{image.id}")
+    end
 
-  # @param [String] id
-  # @return [Hash]
-  def self.info(id)
-    Riiif::Image.new(id).info
+    # @param [String] id
+    # @return [Hash]
+    def self.info(id)
+      Riiif::Image.new(id).info
+    end
   end
 end
