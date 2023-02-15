2033e5e1e901f40334d374b9fa82b533b698f1f6
Justin Coyne <jcoyne@justincoyne.com>
2023-02-15 09:58:25 -0600

    Make leaflet an NPM dependency

--- app/assets/javascripts/spotlight/admin/index.js
+++ app/assets/javascripts/spotlight/admin/index.js
@@ -6,7 +6,7 @@
 //= require bootstrap-tagsinput
 //= require jquery.serializejson
 //= require clipboard/dist/clipboard
-//= require leaflet
+//= require leaflet/dist/leaflet-src
 //= require leaflet-iiif
 //= require Leaflet.Editable
 //= require Path.Drag
