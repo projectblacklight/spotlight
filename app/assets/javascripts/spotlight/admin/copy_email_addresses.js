11bd174b8916efd5d41ec2613f19d25049ee8988
Justin Coyne <jcoyne@justincoyne.com>
2023-02-16 08:44:42 -0600

    Remove jquery dependency for copy_email_addresses.js (#2894)

--- app/assets/javascripts/spotlight/admin/copy_email_addresses.js
+++ app/assets/javascripts/spotlight/admin/copy_email_addresses.js
@@ -1,9 +1,3 @@
-(function($) {
-    $.fn.copyEmailAddresses = function( options ) {
-        var clip = new Clipboard('.copy-email-addresses');
-    };
-})( jQuery );
-
 Spotlight.onLoad(function() {
-    $('.copy-email-addresses').copyEmailAddresses();
+    new Clipboard('.copy-email-addresses');
 });
