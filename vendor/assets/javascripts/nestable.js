4a5985bb05e1fce5cc73f19318440108a99c8a8c
Justin Coyne <jcoyne@justincoyne.com>
2023-02-17 09:40:54 -0600

    Declare undeclared variables

--- vendor/assets/javascripts/nestable.js
+++ vendor/assets/javascripts/nestable.js
@@ -185,7 +185,7 @@
             var data,
                 depth = 0,
                 list  = this;
-                step  = function(level, depth)
+                const step  = function(level, depth)
                 {
                     var array = [ ],
                         items = level.children(list.options.itemNodeName);
