cb09974930a1c0b2f6b0ee995baebdee59619611
Justin Coyne <jcoyne@justincoyne.com>
2023-02-22 16:44:57 -0600

    rails-ujs is deprecated so stop depending on it

--- app/javascript/spotlight/spotlight.js
+++ app/javascript/spotlight/spotlight.js
@@ -10,6 +10,9 @@ const Spotlight = function() {
         buffer[i].call();
       }
     },
+    csrfToken: function () {
+      return document.querySelector('meta[name=csrf-token]')?.content
+    },
     ZprLinks: {
       close: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z\"/></svg>",
       zoomIn: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm.5-7H9v2H7v1h2v2h1v-2h2V9h-2z\"/></svg>\n",
