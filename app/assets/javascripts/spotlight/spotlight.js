94e309630c27b841a1c1225195c5d2b1948e5868
Justin Coyne <jcoyne@justincoyne.com>
2023-02-22 16:44:57 -0600

    rails-ujs is deprecated so stop depending on it

--- app/assets/javascripts/spotlight/spotlight.js
+++ app/assets/javascripts/spotlight/spotlight.js
@@ -4348,6 +4348,36 @@
     input.parent().prev('[data-behavior="non-iiif-alert"]').hide();
   }
 
+  const Spotlight$1 = function() {
+    var buffer = [];
+    return {
+      onLoad: function(func) {
+        buffer.push(func);
+      },
+
+      activate: function() {
+        for(var i = 0; i < buffer.length; i++) {
+          buffer[i].call();
+        }
+      },
+      csrfToken: function () {
+        return document.querySelector('meta[name=csrf-token]')?.content
+      },
+      ZprLinks: {
+        close: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z\"/></svg>",
+        zoomIn: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm.5-7H9v2H7v1h2v2h1v-2h2V9h-2z\"/></svg>\n",
+        zoomOut: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7V9z\"/></svg>\n"
+      }
+    };
+  }();
+
+  // This allows us to configure Spotlight in app/views/layouts/base.html.erb
+  window.Spotlight = Spotlight$1;
+
+  Blacklight.onLoad(function() {
+    Spotlight$1.activate();
+  });
+
   class Crop {
     constructor(cropArea) {
       this.cropArea = cropArea;
@@ -4601,8 +4631,6 @@
     }
 
     uploadFile() {
-      // Set a ujs adapter to support both rails-ujs and jquery-ujs
-      var ujs = typeof Rails === 'undefined' ? $.rails : Rails;
       var url = this.fileInput.data('endpoint');
       // Every post creates a new image/masthead.
       // Because they create IIIF urls which are heavily cached.
@@ -4614,7 +4642,7 @@
         // Form data
         data: this.getData(),
         headers: {
-          'X-CSRF-Token': ujs.csrfToken() || ''
+          'X-CSRF-Token': Spotlight$1.csrfToken() || ''
         },
         //Options to tell jQuery not to process data or worry about content-type.
         cache: false,
@@ -5076,45 +5104,16 @@
     });
   };
 
-  const Spotlight$1 = function() {
-    var buffer = [];
-    return {
-      onLoad: function(func) {
-        buffer.push(func);
-      },
-
-      activate: function() {
-        for(var i = 0; i < buffer.length; i++) {
-          buffer[i].call();
-        }
-      },
-      ZprLinks: {
-        close: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z\"/></svg>",
-        zoomIn: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zm.5-7H9v2H7v1h2v2h1v-2h2V9h-2z\"/></svg>\n",
-        zoomOut: "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"24\" viewBox=\"0 0 24 24\" width=\"24\"><path d=\"M0 0h24v24H0V0z\" fill=\"none\"/><path d=\"M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14zM7 9h5v1H7V9z\"/></svg>\n"
-      }
-    };
-  }();
-
-  // This allows us to configure Spotlight in app/views/layouts/base.html.erb
-  window.Spotlight = Spotlight$1;
-
-  Blacklight.onLoad(function() {
-    Spotlight$1.activate();
-  });
-
   // Place all the behaviors and hooks related to the matching controller here.
 
   class Pages {
     connect(){
-      // Set a ujs adapter to support both rails-ujs and jquery-ujs
-      var ujs = typeof Rails === 'undefined' ? $.rails : Rails;
       SirTrevor.setDefaults({
         iconUrl: Spotlight$1.sirTrevorIcon,
         uploadUrl: $('[data-attachment-endpoint]').data('attachment-endpoint'),
         ajaxOptions: {
           headers: {
-            'X-CSRF-Token': ujs.csrfToken() || ''
+            'X-CSRF-Token': Spotlight$1.csrfToken() || ''
           },
           credentials: 'same-origin'
         }
