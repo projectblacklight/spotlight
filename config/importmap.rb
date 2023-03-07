f91ac345d93ec1f8d4c70990f6f0d3ac4ef574f2
Justin Coyne <jcoyne@justincoyne.com>
2023-03-07 14:26:29 -0600

    Add the vendor javascripts to the manifest

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,4 @@
 # frozen_string_literal: true
 
 pin_all_from File.expand_path('../app/javascript/spotlight', __dir__), under: 'spotlight'
+pin_all_from File.expand_path('../vendor/assets/javascripts', __dir__)
