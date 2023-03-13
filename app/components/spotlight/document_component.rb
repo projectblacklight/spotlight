a858bf25534da9adc0c46906a345458d6eb976f8
Justin Coyne <jcoyne@justincoyne.com>
2023-03-13 13:08:53 -0500

    Make the edit button display in the right location on Blacklight 8 (#2969)
    On Blacklight 8 the partials render at the bottom of the component, but we want the edit button in the header.

---
+++ app/components/spotlight/document_component.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Spotlight
+  # Displays the document
+  # This overrides the title method to provide an edit link.
+  class DocumentComponent < Blacklight::DocumentComponent
+    def title
+      safe_join([exhibit_edit_link, super, add_document_meta_content(@document)])
+    end
+
+    attr_reader :document
+
+    delegate :current_exhibit, :can?, :add_document_meta_content, to: :helpers
+
+    def exhibit_edit_link
+      helpers.exhibit_edit_link document, [:edit, current_exhibit, document], class: 'float-right btn btn-primary' if can?(:curate, current_exhibit)
+    end
+  end
+end
