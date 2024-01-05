8921802ded514d971d94caeedc38bc65787a7b79
Justin Coyne <jcoyne@justincoyne.com>
2024-01-05 15:23:10 -0600

    Allow paper_trail up though version 15

--- lib/spotlight/engine.rb
+++ lib/spotlight/engine.rb
@@ -334,10 +334,23 @@ module Spotlight
     config.assign_default_roles_to_first_user = true
 
     config.exhibit_roles = %w[admin curator viewer]
-
-    if ActiveRecord.respond_to?(:yaml_column_permitted_classes) || ActiveRecord::Base.respond_to?(:yaml_column_permitted_classes)
-      config.active_record.yaml_column_permitted_classes ||= []
-      config.active_record.yaml_column_permitted_classes += [Symbol, ActiveSupport::HashWithIndifferentAccess]
+    # PaperTrail serializes objects to YAML, so we need to permit these classes to be deserialized
+    if ActiveRecord.respond_to?(:yaml_column_permitted_classes)
+      # Rails >= 7.0
+      ActiveRecord.yaml_column_permitted_classes ||= []
+      ActiveRecord.yaml_column_permitted_classes += [Symbol,
+                                                     ActiveSupport::HashWithIndifferentAccess,
+                                                     ActiveSupport::TimeWithZone,
+                                                     ActiveSupport::TimeZone,
+                                                     Time]
+    elsif ActiveRecord::Base.respond_to?(:yaml_column_permitted_classes)
+      # Rails 6.1
+      ActiveRecord::Base.yaml_column_permitted_classes ||= []
+      ActiveRecord::Base.yaml_column_permitted_classes += [Symbol,
+                                                           ActiveSupport::HashWithIndifferentAccess,
+                                                           ActiveSupport::TimeWithZone,
+                                                           ActiveSupport::TimeZone,
+                                                           Time]
     end
   end
   # rubocop:enable Metrics/ClassLength
