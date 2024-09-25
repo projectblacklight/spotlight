f3eaaf1113326002eb5641edc61b5b782641cef9
Steve Taylor <sctaylor@stanford.edu>
2024-09-25 16:32:20 -0700

    Remove local copy of CheckboxSubmit
    If we force at least Blacklight 7.38 and use the ESM module, Blacklight
    handles this.

--- app/assets/javascripts/spotlight/spotlight.esm.js
+++ app/assets/javascripts/spotlight/spotlight.esm.js
@@ -1,5 +1,6 @@
 import require$$0 from 'leaflet';
 import Clipboard from 'clipboard';
+import Blacklight from 'blacklight-frontend';
 
 // Includes an unreleased RTL support pull request: https://github.com/ganlanyuan/tiny-slider/pull/658
 // Includes "export default tns" at the end of the file for spotlight/user/browse_group_categories.js
@@ -31689,99 +31690,17 @@ class TranslationProgress {
   }
 }
 
-/*
-NOTE: this is copied & adapted from BL8's checkbox_submit.js in order to have
-it accessible in a BL7-based spotlight. Once we drop support for BL7, this file
-can be deleted and we can change visibility_toggle.es6 to import CheckboxSubmit
-from Blacklight.
-
-See https://github.com/projectblacklight/blacklight/blob/main/app/javascript/blacklight/checkbox_submit.js
-*/
-class CheckboxSubmit {
-  constructor(form) {
-    this.form = form;
-  }
-
-  async clicked(evt) {
-    this.spanTarget.innerHTML = this.form.getAttribute('data-inprogress');
-    this.labelTarget.setAttribute('disabled', 'disabled');
-    this.checkboxTarget.setAttribute('disabled', 'disabled');
-    const csrfMeta = document.querySelector('meta[name=csrf-token]');
-    const response = await fetch(this.formTarget.getAttribute('action'), {
-      body: new FormData(this.formTarget),
-      method: this.formTarget.getAttribute('method').toUpperCase(),
-      headers: {
-        'Accept': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-Token': csrfMeta ? csrfMeta.content : ''
-      }
-    });
-    this.labelTarget.removeAttribute('disabled');
-    this.checkboxTarget.removeAttribute('disabled');
-    if (response.ok) {
-      this.updateStateFor(!this.checked);
-      // Not used for our case in Spotlight (visibility toggle)
-      // const json = await response.json()
-      // document.querySelector('[data-role=bookmark-counter]').innerHTML = json.bookmarks.count
-    } else {
-      alert('Error');
-    }
-  }
-
-  get checked() {
-    return (this.form.querySelectorAll('input[name=_method][value=delete]').length != 0)
-  }
-
-  get formTarget() {
-    return this.form
-  }
-
-  get labelTarget() {
-    return this.form.querySelector('[data-checkboxsubmit-target="label"]')
-  }
-
-  get checkboxTarget() {
-    return this.form.querySelector('[data-checkboxsubmit-target="checkbox"]')
-  }
-
-  get spanTarget() {
-    return this.form.querySelector('[data-checkboxsubmit-target="span"]')
-  }
-
-  updateStateFor(state) {
-    this.checkboxTarget.checked = state;
-
-    if (state) {
-      this.labelTarget.classList.add('checked');
-      //Set the Rails hidden field that fakes an HTTP verb
-      //properly for current state action.
-      this.formTarget.querySelector('input[name=_method]').value = 'delete';
-      this.spanTarget.innerHTML = this.form.getAttribute('data-present');
-    } else {
-      this.labelTarget.classList.remove('checked');
-      this.formTarget.querySelector('input[name=_method]').value = 'put';
-      this.spanTarget.innerHTML = this.form.getAttribute('data-absent');
-    }
-  }
-}
-
-// Visibility toggle for items in an exhibit, based on Blacklight's bookmark toggle
-
+// Blacklight's BookmarkToggle is doing the real work, this only adds/removes the "blacklight-private" class.
 const VisibilityToggle = (e) => {
   if (e.target.matches('[data-checkboxsubmit-target="checkbox"]')) {
     const form = e.target.closest('form');
     if (form) {
-      new CheckboxSubmit(form).clicked(e);
-
       // Add/remove the "private" label to the document row when visibility is toggled
       const docRow = form.closest('tr');
       if (docRow) docRow.classList.toggle('blacklight-private');
     }
   }
 };
-
-VisibilityToggle.selector = 'form.visibility-toggle';
-
 document.addEventListener('click', VisibilityToggle);
 
 class Users {
--- app/assets/javascripts/spotlight/spotlight.js
+++ app/assets/javascripts/spotlight/spotlight.js
@@ -1,13 +1,14 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('leaflet'), require('clipboard')) :
-  typeof define === 'function' && define.amd ? define(['leaflet', 'clipboard'], factory) :
-  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Spotlight = factory(global.L, global.Clipboard));
-})(this, (function (require$$0, Clipboard) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('leaflet'), require('clipboard'), require('blacklight-frontend')) :
+  typeof define === 'function' && define.amd ? define(['leaflet', 'clipboard', 'blacklight-frontend'], factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.Spotlight = factory(global.L, global.Clipboard, global.Blacklight));
+})(this, (function (require$$0, Clipboard, Blacklight) { 'use strict';
 
   const _interopDefaultLegacy = e => e && typeof e === 'object' && 'default' in e ? e : { default: e };
 
   const require$$0__default = /*#__PURE__*/_interopDefaultLegacy(require$$0);
   const Clipboard__default = /*#__PURE__*/_interopDefaultLegacy(Clipboard);
+  const Blacklight__default = /*#__PURE__*/_interopDefaultLegacy(Blacklight);
 
   // Includes an unreleased RTL support pull request: https://github.com/ganlanyuan/tiny-slider/pull/658
   // Includes "export default tns" at the end of the file for spotlight/user/browse_group_categories.js
@@ -7271,7 +7272,7 @@
   // This allows us to configure Spotlight in app/views/layouts/base.html.erb
   window.Spotlight = Spotlight$1;
 
-  Blacklight.onLoad(function() {
+  Blacklight__default.default.onLoad(function() {
     Spotlight$1.activate();
   });
 
@@ -31697,99 +31698,17 @@
     }
   }
 
-  /*
-  NOTE: this is copied & adapted from BL8's checkbox_submit.js in order to have
-  it accessible in a BL7-based spotlight. Once we drop support for BL7, this file
-  can be deleted and we can change visibility_toggle.es6 to import CheckboxSubmit
-  from Blacklight.
-
-  See https://github.com/projectblacklight/blacklight/blob/main/app/javascript/blacklight/checkbox_submit.js
-  */
-  class CheckboxSubmit {
-    constructor(form) {
-      this.form = form;
-    }
-
-    async clicked(evt) {
-      this.spanTarget.innerHTML = this.form.getAttribute('data-inprogress');
-      this.labelTarget.setAttribute('disabled', 'disabled');
-      this.checkboxTarget.setAttribute('disabled', 'disabled');
-      const csrfMeta = document.querySelector('meta[name=csrf-token]');
-      const response = await fetch(this.formTarget.getAttribute('action'), {
-        body: new FormData(this.formTarget),
-        method: this.formTarget.getAttribute('method').toUpperCase(),
-        headers: {
-          'Accept': 'application/json',
-          'X-Requested-With': 'XMLHttpRequest',
-          'X-CSRF-Token': csrfMeta ? csrfMeta.content : ''
-        }
-      });
-      this.labelTarget.removeAttribute('disabled');
-      this.checkboxTarget.removeAttribute('disabled');
-      if (response.ok) {
-        this.updateStateFor(!this.checked);
-        // Not used for our case in Spotlight (visibility toggle)
-        // const json = await response.json()
-        // document.querySelector('[data-role=bookmark-counter]').innerHTML = json.bookmarks.count
-      } else {
-        alert('Error');
-      }
-    }
-
-    get checked() {
-      return (this.form.querySelectorAll('input[name=_method][value=delete]').length != 0)
-    }
-
-    get formTarget() {
-      return this.form
-    }
-
-    get labelTarget() {
-      return this.form.querySelector('[data-checkboxsubmit-target="label"]')
-    }
-
-    get checkboxTarget() {
-      return this.form.querySelector('[data-checkboxsubmit-target="checkbox"]')
-    }
-
-    get spanTarget() {
-      return this.form.querySelector('[data-checkboxsubmit-target="span"]')
-    }
-
-    updateStateFor(state) {
-      this.checkboxTarget.checked = state;
-
-      if (state) {
-        this.labelTarget.classList.add('checked');
-        //Set the Rails hidden field that fakes an HTTP verb
-        //properly for current state action.
-        this.formTarget.querySelector('input[name=_method]').value = 'delete';
-        this.spanTarget.innerHTML = this.form.getAttribute('data-present');
-      } else {
-        this.labelTarget.classList.remove('checked');
-        this.formTarget.querySelector('input[name=_method]').value = 'put';
-        this.spanTarget.innerHTML = this.form.getAttribute('data-absent');
-      }
-    }
-  }
-
-  // Visibility toggle for items in an exhibit, based on Blacklight's bookmark toggle
-
+  // Blacklight's BookmarkToggle is doing the real work, this only adds/removes the "blacklight-private" class.
   const VisibilityToggle = (e) => {
     if (e.target.matches('[data-checkboxsubmit-target="checkbox"]')) {
       const form = e.target.closest('form');
       if (form) {
-        new CheckboxSubmit(form).clicked(e);
-
         // Add/remove the "private" label to the document row when visibility is toggled
         const docRow = form.closest('tr');
         if (docRow) docRow.classList.toggle('blacklight-private');
       }
     }
   };
-
-  VisibilityToggle.selector = 'form.visibility-toggle';
-
   document.addEventListener('click', VisibilityToggle);
 
   class Users {
