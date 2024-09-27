a5578f31336ee1a9103cdf8a7d22e9e5720ec9b9
Chris Beer <cabeer@stanford.edu>
2024-09-27 09:04:26 -0700

    Update icons work-around for compatibility with blacklight 7.39 (which backported the 7.38 fix.)

--- app/components/spotlight/icon_component.rb
+++ app/components/spotlight/icon_component.rb
@@ -3,10 +3,10 @@
 module Spotlight
   # Displays the document
   class IconComponent < Blacklight::Icons::IconComponent
-    if Blacklight.version < '8.0'
-      # Work around https://github.com/projectblacklight/blacklight/issues/3232 (fixed in Blacklight 8.0)
+    if Blacklight.version < '7.39'
+      # Work around https://github.com/projectblacklight/blacklight/issues/3232 (fixed in Blacklight 7.39)
       def classes
-        (@classes - ['blacklight-icons-'] + ["blacklight-icons-#{name}"]).uniq
+        ((@classes || (super if defined?(super)) || []) - ['blacklight-icons-'] + ["blacklight-icons-#{name}"]).uniq
       end
     end
   end
