ea738ff5cf569d5029132e32b76b31d5178d8d41
Steve Taylor <sctaylor@stanford.edu>
2024-10-16 16:26:40 -0700

    Rubocop

--- app/models/spotlight/custom_search_field.rb
+++ app/models/spotlight/custom_search_field.rb
@@ -17,7 +17,7 @@ module Spotlight
     end
 
     def label
-      conf = if slug && blacklight_configuration && blacklight_configuration.search_fields.key?(slug)
+      conf = if slug && blacklight_configuration&.search_fields&.key?(slug)
                blacklight_configuration.search_fields[slug].reverse_merge(configuration)
              else
                configuration
@@ -32,7 +32,7 @@ module Spotlight
     end
 
     def update_blacklight_configuration_label(label)
-      return unless slug && blacklight_configuration && blacklight_configuration.search_fields.key?(slug)
+      return unless slug && blacklight_configuration&.search_fields&.key?(slug)
 
       blacklight_configuration.search_fields[slug]['label'] = label
       blacklight_configuration.save
