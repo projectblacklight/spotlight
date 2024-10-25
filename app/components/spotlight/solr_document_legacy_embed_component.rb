349733b63432d3cb2f776b065df69a8b1a6ec1d9
Chris Beer <cabeer@stanford.edu>
2024-10-25 14:27:03 -0700

    Pass the block context through to the embed component

--- app/components/spotlight/solr_document_legacy_embed_component.rb
+++ app/components/spotlight/solr_document_legacy_embed_component.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
-# Render an document suitable for embedding on a feature page.
 module Spotlight
+  # Render an document suitable for embedding on a feature page.
   class SolrDocumentLegacyEmbedComponent < Blacklight::DocumentComponent
+    attr_reader :block_context
+
+    def initialize(*args, block: nil, **kwargs)
+      super
+
+      @block_context = block
+    end
+
+    def before_render
+      set_slot(:embed, nil, block_context: block_context) unless embed
+
+      super
+    end
   end
 end
