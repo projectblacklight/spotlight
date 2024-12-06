fc9d77f8cab4e19722279546ed2c04a63bdbcea7
Justin Coyne <jcoyne@justincoyne.com>
2024-12-06 16:28:52 -0600

    Extract the HeaderNavigationLinkComponent
    We will find this helpful in our app, because we can just override this component rather than have to copy 3 partials into our local app just to change some classes in the markup

---
+++ spec/components/spotlight/header_navigation_link_component_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe Spotlight::HeaderNavigationLinkComponent, type: :component do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  # it "renders something useful" do
+  #   expect(
+  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
+  #   ).to include(
+  #     "Hello, components!"
+  #   )
+  # end
+end
