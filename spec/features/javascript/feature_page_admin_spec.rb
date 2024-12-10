0b20c3d4b86b3a53bfc914571aee69796c3ddfc1
Cory Lown <corylown@stanford.edu>
2024-12-10 10:47:28 -0500

    Correct comment

--- spec/features/javascript/feature_page_admin_spec.rb
+++ spec/features/javascript/feature_page_admin_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe 'Feature Pages Adminstration', js: true do
   end
 
   it 'stays in curation mode if a user has unsaved data' do
-    skip('Chromedriver automatically dismisses alerts so cancel button cannot be found')
+    skip('Chromedriver automatically dismisses alerts so this test does not work')
 
     visit spotlight.edit_exhibit_feature_page_path(page1.exhibit, page1)
 
@@ -68,7 +68,7 @@ RSpec.describe 'Feature Pages Adminstration', js: true do
   end
 
   it 'stays in curation mode if a user has unsaved contenteditable data' do
-    skip('Chromedriver automatically dismisses alerts so cancel button cannot be found')
+    skip('Chromedriver automatically dismisses alerts so this test does not work')
 
     visit spotlight.edit_exhibit_feature_page_path(page1.exhibit, page1)
 
