ea0377cc9b87bd1fbc10504309323757f2f9ccd7
Cory Lown <corylown@stanford.edu>
2025-02-12 14:02:13 -0500

    Drop BL 7 support

--- spec/test_app_templates/catalog_controller.rb
+++ spec/test_app_templates/catalog_controller.rb
@@ -13,19 +13,12 @@ class CatalogController < ApplicationController
     config.view.masonry(document_component: Blacklight::Gallery::DocumentComponent, icon: Blacklight::Gallery::Icons::MasonryComponent)
     config.view.slideshow(document_component: Blacklight::Gallery::SlideshowComponent, icon: Blacklight::Gallery::Icons::SlideshowComponent)
 
-    if Blacklight.version > '8.0'
-      config.view.embed(if: false,
-                        partials: [],
-                        document_component: Spotlight::SolrDocumentLegacyEmbedComponent,
-                        embed_component: Blacklight::Gallery::OpenseadragonEmbedComponent)
-
-      config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
-
-      config.show.embed_component = Blacklight::Gallery::OpenseadragonEmbedComponent
-    else
-      config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
-      config.show.partials.insert(1, :openseadragon)
-    end
+    config.view.embed(if: false,
+                      partials: [],
+                      document_component: Spotlight::SolrDocumentLegacyEmbedComponent,
+                      embed_component: Blacklight::Gallery::OpenseadragonEmbedComponent)
+    config.show.tile_source_field = :content_metadata_image_iiif_info_ssm
+    config.show.embed_component = Blacklight::Gallery::OpenseadragonEmbedComponent
 
     ## Default parameters to send to solr for all search-like requests. See also SolrHelper#solr_search_params
     config.default_solr_params = {
