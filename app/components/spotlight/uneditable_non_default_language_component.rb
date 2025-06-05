60a33fb699382711d5f0fa3c2f74d4aa00fc2595
Cory Lown <corylown@stanford.edu>
2025-06-05 16:04:39 -0400

    Do not allow editing of translatable fields when a locale other than the default is set

---
+++ app/components/spotlight/uneditable_non_default_language_component.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Spotlight
+  # Displays a message that this exhibit information cannot be edited in the
+  # currently selected language and provides other options.
+  class UneditableNonDefaultLanguageComponent < ViewComponent::Base
+    def initialize(current_exhibit:, current_language:)
+      @current_exhibit = current_exhibit
+      @current_language = current_language
+      super
+    end
+
+    def edit_translations_button
+      link_to I18n.t('spotlight.exhibits.form.uneditable_non_default_language_form.translations'),
+              helpers.spotlight.edit_exhibit_translations_path(@current_exhibit, locale: @current_language),
+              class: 'btn btn-primary'
+    end
+
+    def switch_to_default_language_button
+      link_to I18n.t('spotlight.exhibits.form.uneditable_non_default_language_form.default_language'),
+              helpers.spotlight.edit_exhibit_path(@current_exhibit, locale: I18n.default_locale),
+              class: 'btn btn-primary'
+    end
+  end
+end
--- spec/views/spotlight/pages/index.html.erb_spec.rb
+++ spec/views/spotlight/pages/index.html.erb_spec.rb
@@ -14,11 +14,13 @@ RSpec.describe 'spotlight/pages/index.html.erb', type: :view do
     ]
   end
   let(:exhibit) { stub_model(Spotlight::Exhibit) }
+  let(:default_language) { true }
 
   before do
     allow(view).to receive(:page_collection_name).and_return(:feature_pages)
     allow(view).to receive(:exhibit_alt_text_path).and_return('/')
     allow(view).to receive(:update_all_exhibit_feature_pages_path).and_return('/exhibit/features/update_all')
+    allow(view).to receive(:default_language?).and_return(default_language)
     assign(:page, Spotlight::FeaturePage.new)
     assign(:exhibit, exhibit)
     allow(view).to receive(:current_exhibit).and_return(exhibit)
@@ -38,6 +40,22 @@ RSpec.describe 'spotlight/pages/index.html.erb', type: :view do
       render
       expect(rendered).to have_no_selector 'button[disabled]', text: 'Save changes'
       expect(rendered).to have_selector 'button', text: 'Save changes'
+    end
+  end
+
+  describe 'when a locale other than the default is set' do
+    let(:default_language) { false }
+
+    before do
+      assign(:pages, pages)
+      allow(exhibit).to receive(:feature_pages).and_return pages
+      render
+    end
+
+    it 'removes the form and displays a translations message' do
+      expect(rendered).to have_text 'Please use the translations editor'
+      expect(rendered).to have_no_selector '.card-title', text: 'Title1'
+      expect(rendered).to have_no_selector '.card-title', text: 'Title2'
     end
   end
 end
