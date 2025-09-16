db554d7442ff50519d84ba7d1bbf62b02d37738e
Sarah Chintomby <sac356@cornell.edu>
2025-09-16 17:44:27 -0400

    rebased, and fixed the initial state when carousel loads the aria described by attributes are added

--- app/javascript/spotlight/user/carousel.js
+++ app/javascript/spotlight/user/carousel.js
@@ -1,19 +1,31 @@
 export default class {
   connect() {
     if ($.fn.carousel) {
-      $('.carousel').carousel();
+      const $carousel = $('.carousel');
 
-      // when slide changes, update the aria-describedby on the next and prev btns
-      $('.carousel').on('slid.bs.carousel', function () {
-        var $activeItem = $(this).find('.carousel-item.active');
-        var prevId = $activeItem.data('prev-id');
-        var nextId = $activeItem.data('next-id');
+      // updates the aria-describedby on the next and prev btns
+      const updateAriaDescribedBy = function ($carousel) {
+        const $activeItem = $carousel.find('.carousel-item.active');
+        const prevId = $activeItem.data('prev-id');
+        const nextId = $activeItem.data('next-id');
         if (prevId) {
-          $(this).find('.carousel-control-prev').attr('aria-describedby', prevId);
+          $carousel.find('.carousel-control-prev').attr('aria-describedby', prevId);
         }
         if (nextId) {
-          $(this).find('.carousel-control-next').attr('aria-describedby', nextId);
+          $carousel.find('.carousel-control-next').attr('aria-describedby', nextId);
         }
+      };
+
+      // on initial page load, set the aria-describedby on the btns for each carousel
+      $carousel.each(function () {
+        const $this = $(this);
+        $this.carousel();
+        updateAriaDescribedBy($this);
+      });
+
+      // on slide change
+      $carousel.on('slid.bs.carousel', function () {
+        updateAriaDescribedBy($(this));
       });
     }
   }
