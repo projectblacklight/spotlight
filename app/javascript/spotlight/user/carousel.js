b6c7b9cea91a60781246ffebcf730db0a087644c
Sarah Chintomby <sac356@cornell.edu>
2025-09-16 15:32:04 -0400

    adding aria attributes to carousel buttons to improve accessibilty

--- app/javascript/spotlight/user/carousel.js
+++ app/javascript/spotlight/user/carousel.js
@@ -2,6 +2,19 @@ export default class {
   connect() {
     if ($.fn.carousel) {
       $('.carousel').carousel();
+
+      // when slide changes, update the aria-describedby on the next and prev btns
+      $('.carousel').on('slid.bs.carousel', function () {
+        var $activeItem = $(this).find('.carousel-item.active');
+        var prevId = $activeItem.data('prev-id');
+        var nextId = $activeItem.data('next-id');
+        if (prevId) {
+          $(this).find('.carousel-control-prev').attr('aria-describedby', prevId);
+        }
+        if (nextId) {
+          $(this).find('.carousel-control-next').attr('aria-describedby', nextId);
+        }
+      });
     }
   }
 }
