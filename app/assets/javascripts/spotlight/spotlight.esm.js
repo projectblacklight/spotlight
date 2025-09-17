06fcb7c4e1972915c588ffd693ba20e4ac4307b4
Sarah Chintomby <sac356@cornell.edu>
2025-09-17 10:56:03 -0400

    adding generated spotlight js files to support carousel changes

--- app/assets/javascripts/spotlight/spotlight.esm.js
+++ app/assets/javascripts/spotlight/spotlight.esm.js
@@ -3266,7 +3266,32 @@ class BrowseGroupCateogries {
 class Carousel {
   connect() {
     if ($.fn.carousel) {
-      $('.carousel').carousel();
+      const $carousel = $('.carousel');
+
+      // updates the aria-describedby on the next and prev btns
+      const updateAriaDescribedBy = function ($carousel) {
+        const $activeItem = $carousel.find('.carousel-item.active');
+        const prevId = $activeItem.data('prev-id');
+        const nextId = $activeItem.data('next-id');
+        if (prevId) {
+          $carousel.find('.carousel-control-prev').attr('aria-describedby', prevId);
+        }
+        if (nextId) {
+          $carousel.find('.carousel-control-next').attr('aria-describedby', nextId);
+        }
+      };
+
+      // on initial page load, set the aria-describedby on the btns for each carousel
+      $carousel.each(function () {
+        const $this = $(this);
+        $this.carousel();
+        updateAriaDescribedBy($this);
+      });
+
+      // on slide change
+      $carousel.on('slid.bs.carousel', function () {
+        updateAriaDescribedBy($(this));
+      });
     }
   }
 }
