62cd334019349c3ebb68e8910a04eb538cc559a0
Emily Toner <ebtoner@users.noreply.github.com>
2025-09-19 10:41:45 -0400

    Remove Github Markup and add RedCarpet markdown renderer with automatic heading ids (#3563)
    * Remove Github Markup and add RedCarpet markdown renderer with automatic heading ids
    
    * Comment edit
    
    * Small test edit
    
    * Remove sanitize

--- app/helpers/spotlight/rendering_helper.rb
+++ app/helpers/spotlight/rendering_helper.rb
@@ -3,7 +3,10 @@
 module Spotlight
   module RenderingHelper # :nodoc:
     def render_markdown(text)
-      GitHub::Markup.render('.md', text).html_safe
+      # Extend Redcarpet renderer to add HTML anchors to each heading in the output HTML
+      renderer = Redcarpet::Render::HTML.new(with_toc_data: true)
+      # Use Redcarpet to render markdown as html
+      Redcarpet::Markdown.new(renderer).render(text).html_safe
     end
   end
 end
---
+++ spec/helpers/spotlight/rendering_helper_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+RSpec.describe Spotlight::RenderingHelper, type: :helper do
+  describe '#render_markdown' do
+    it 'renders basic markdown as html' do
+      expect(helper.render_markdown('Here is some **styled text**.').chomp).to match(%r{<p>Here is some <strong>styled text</strong>.</p>})
+    end
+
+    it 'add heading ids automatically' do
+      expect(helper.render_markdown('## Test Header').chomp).to match(%r{<h2 id="test-header">Test Header</h2>})
+    end
+  end
+end
