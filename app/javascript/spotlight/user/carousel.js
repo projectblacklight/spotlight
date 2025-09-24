fd189492f942837704f1a688f62bb78cbf4b723b
sarah-cul <144364984+sarah-cul@users.noreply.github.com>
2025-09-24 11:10:32 -0400

    3529 carousel aria attributes (#3560)
    * adding aria attributes to carousel buttons to improve accessibilty
    
    * rebased, and fixed the initial state when carousel loads the aria described by attributes are added
    
    * add a new carousel spec test and normalize the yml file change
    
    * adding generated spotlight js files to support carousel changes
    
    * moving logic for the carousel aria-describedby to javascript

--- app/javascript/spotlight/user/carousel.js
+++ app/javascript/spotlight/user/carousel.js
@@ -1,7 +1,37 @@
 export default class {
   connect() {
     if ($.fn.carousel) {
-      $('.carousel').carousel();
+      const $carousel = $('.carousel');
+
+      // updates the aria-describedby on the next and prev btns
+      const updateAriaDescribedBy = function ($carousel) {
+        const $activeItem = $carousel.find('.carousel-item.active');
+        const $items = $carousel.find('.carousel-item');
+        const curIndex = $items.index($activeItem);
+        const prevIndex = (curIndex - 1 + $items.length) % $items.length;
+        const nextIndex = (curIndex + 1) % $items.length;
+
+        const prevDataId = $items.eq(prevIndex).data('id');
+        const nextDataId = $items.eq(nextIndex).data('id');
+        if (prevDataId) {
+          $carousel.find('.carousel-control-prev').attr('aria-describedby', 'carousel-caption-' + prevDataId);
+        }
+        if (nextDataId) {
+          $carousel.find('.carousel-control-next').attr('aria-describedby', 'carousel-caption-' + nextDataId);
+        }
+      };
+
+      // on initial page load, set the aria-describedby on the btns for each carousel
+      $carousel.each(function () {
+        const $this = $(this);
+        $this.carousel();
+        updateAriaDescribedBy($this);
+      });
+
+      // on slide change
+      $carousel.on('slid.bs.carousel', function () {
+        updateAriaDescribedBy($(this));
+      });
     }
   }
 }
