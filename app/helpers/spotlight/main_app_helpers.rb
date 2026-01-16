0047d29668d9ae573ddf434d4b251c810c03b2f5
Steve Taylor <sctaylor@stanford.edu>
2026-01-16 11:00:44 -0800

    Consolidate exhibit theme helpers into a module

--- app/helpers/spotlight/main_app_helpers.rb
+++ app/helpers/spotlight/main_app_helpers.rb
@@ -5,6 +5,7 @@ module Spotlight
   # Helpers that are injected into the main application (because they used in layouts)
   module MainAppHelpers
     include Blacklight::DocumentHelperBehavior
+    include Spotlight::ExhibitThemeHelper
     include Spotlight::NavbarHelper
     include Spotlight::MastheadHelper
 
@@ -36,19 +37,6 @@ module Spotlight
       else
         blacklight_config.view_config(action_name: :show).document_presenter_class
       end
-    end
-
-    def exhibit_stylesheet_link_tag(tag)
-      if current_exhibit_theme && current_exhibit&.theme != 'default'
-        stylesheet_link_tag "#{tag}_#{current_exhibit_theme}"
-      else
-        Rails.logger.debug { "Exhibit theme '#{current_exhibit_theme}' not in the list of available themes: #{current_exhibit&.themes}" }
-        stylesheet_link_tag(tag)
-      end
-    end
-
-    def current_exhibit_theme
-      current_exhibit.theme if current_exhibit && current_exhibit.theme.present? && current_exhibit.themes.include?(current_exhibit.theme)
     end
   end
 end
