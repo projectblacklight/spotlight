1438fddff8f6f253617d79e25afa1e5ef98b8061
Justin Coyne <jcoyne@justincoyne.com>
2026-01-29 14:12:09 -0600

    Remove jquery dependency from sir-trevor block mixins

--- app/javascript/spotlight/admin/block_mixins/formable.js
+++ app/javascript/spotlight/admin/block_mixins/formable.js
@@ -1,78 +1,109 @@
-(function ($){
+;(function () {
   SirTrevor.BlockMixins.Formable = {
     mixinName: "Formable",
     preload: true,
 
-    initializeFormable: function() {
-
-      if (this['afterLoadData'] === undefined) {
-        this['afterLoadData'] = function(data) { };
+    initializeFormable: function () {
+      if (this["afterLoadData"] === undefined) {
+        this["afterLoadData"] = function (data) {}
       }
     },
 
-    formId: function(id) {
-      return this.blockID + "_" + id;
+    formId: function (id) {
+      return this.blockID + "_" + id
     },
 
-    _serializeData: function() {
+    _serializeData: function () {
+      const data = {}
+      const formElements = this.inner.querySelectorAll(
+        ":input,textarea,select:not(input[type=radio])"
+      )
 
-      var data = $(":input,textarea,select", this.inner).not(':input:radio').serializeJSON();
+      // Process regular form elements (except radio buttons)
+      formElements.forEach(element => {
+        if (element.name) {
+          // Handle simple case
+          data[element.name] = element.value
+        }
+      })
 
-      $(':input:radio:checked', this.inner).each(function(index, input) {
-        var key = $(input).data('key') || input.getAttribute('name');
-
+      // Process checked radio buttons
+      const checkedRadios = this.inner.querySelectorAll(
+        "input[type=radio]:checked"
+      )
+      checkedRadios.forEach(radio => {
+        const key = radio.dataset.key || radio.getAttribute("name")
         if (!key.match("\\[")) {
-          data[key] = $(input).val();
+          data[key] = radio.value
         }
-      });
+      })
 
-      /* Simple to start. Add conditions later */
+      // Handle text blocks
       if (this.hasTextBlock()) {
-        data.text = this.getTextBlockHTML();
-        data.format = 'html';
-        if (data.text && data.text.length > 0 && this.options.convertToMarkdown) {
-          data.text = stToMarkdown(data.text, this.type);
-          data.format = 'markdown';
+        data.text = this.getTextBlockHTML()
+        data.format = "html"
+        if (
+          data.text &&
+          data.text.length > 0 &&
+          this.options.convertToMarkdown
+        ) {
+          data.text = stToMarkdown(data.text, this.type)
+          data.format = "markdown"
         }
       }
 
-      return data;
+      return data
     },
 
-    loadData: function(data){
+    loadData: function (data) {
       if (this.hasTextBlock()) {
-        if (data.text && data.text.length > 0 && this.options.convertFromMarkdown && data.format !== "html") {
-          this.setTextBlockHTML(SirTrevor.toHTML(data.text, this.type));
+        if (
+          data.text &&
+          data.text.length > 0 &&
+          this.options.convertFromMarkdown &&
+          data.format !== "html"
+        ) {
+          this.setTextBlockHTML(SirTrevor.toHTML(data.text, this.type))
         } else {
-          this.setTextBlockHTML(data.text);
+          this.setTextBlockHTML(data.text)
         }
       }
-      this.loadFormDataByKey(data);
-      this.afterLoadData(data);
+      this.loadFormDataByKey(data)
+      this.afterLoadData(data)
     },
 
-    loadFormDataByKey: function(data) {
-      $(':input', this.inner).not('button,:input[type=hidden]').each(function(index, input) {
-        var key = $(input).data('key') || input.getAttribute('name');
+    loadFormDataByKey: function (data) {
+      const inputs = this.inner.querySelectorAll(
+        ":input:not(button):not([type=hidden])"
+      )
+
+      inputs.forEach(input => {
+        const key = input.dataset.key || input.getAttribute("name")
 
         if (key) {
-
-          if (key.match("\\[\\]$")) {
-            key = key.replace("[]", "");
+          let processedKey = key
+          if (processedKey.match("\\[\\]$")) {
+            processedKey = processedKey.replace("[]", "")
           }
 
-          // by wrapping it in an array, this'll "just work" for radio and checkbox fields too
-          var input_data = data[key];
+          let inputData = data[processedKey]
+          if (inputData !== undefined) {
+            // Convert to array if not already
+            if (!(inputData instanceof Array)) {
+              inputData = [inputData]
+            }
 
-          if (!(input_data instanceof Array)) {
-            input_data = [input_data];
+            // Set value based on input type
+            if (input.type === "checkbox" || input.type === "radio") {
+              input.checked = inputData.includes(input.value)
+            } else {
+              input.value = inputData[0] !== undefined ? inputData[0] : ""
+            }
           }
-          $(this).val(input_data);
         }
-      });
-    },
-  },
+      })
+    }
+  }
 
-
-  SirTrevor.Block.prototype.availableMixins.push("formable");
-})(jQuery);
+  SirTrevor.Block.prototype.availableMixins.push("formable")
+})()
