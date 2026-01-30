9952dc5454ee30b4fb9759c94ae9a0d4fd7cddba
Justin Coyne <jcoyne@justincoyne.com>
2026-01-29 18:43:46 -0600

    Remove jquery dependency from sir-trevor autocomplete block mixin

--- app/assets/javascripts/spotlight/spotlight.esm.js
+++ app/assets/javascripts/spotlight/spotlight.esm.js
@@ -5275,36 +5275,41 @@ class Users {
   }
 }
 
-(function ($){
+(function () {
   SirTrevor.BlockMixins.Autocompleteable = {
     mixinName: "Autocompleteable",
     preload: true,
 
-    initializeAutocompleteable: function() {
+    initializeAutocompleteable: function () {
       this.on("onRender", this.addAutocompletetoSirTrevorForm);
 
-      if (this['autocomplete_url'] === undefined) {
-        this.autocomplete_url = function() { return $('form[data-autocomplete-url]').data('autocomplete-url'); };
-      }
-
-      if (this['autocomplete_fetch'] === undefined) {
-        this.autocomplete_fetch = this.fetchAutocompleteResults;
-      }
-
-      if (this['transform_autocomplete_results'] === undefined) {
-        this.transform_autocomplete_results = (val) => val;
-      }
-
-      if (this['highlight'] === undefined) {
-        this.highlight = function(value) {
-          if (!value) return '';
-          const queryValue = this.getQueryValue().trim();
-          return queryValue ? value.replace(new RegExp(queryValue, 'gi'), '<strong>$&</strong>') : value;
+      if (this["autocomplete_url"] === undefined) {
+        this.autocomplete_url = function () {
+          const form = document.querySelector("form[data-autocomplete-url]");
+          return form ? form.dataset.autocompleteUrl : null
         };
       }
 
-      if (this['autocomplete_control'] === undefined) {
-        this.autocomplete_control = function() {
+      if (this["autocomplete_fetch"] === undefined) {
+        this.autocomplete_fetch = this.fetchAutocompleteResults;
+      }
+
+      if (this["transform_autocomplete_results"] === undefined) {
+        this.transform_autocomplete_results = val => val;
+      }
+
+      if (this["highlight"] === undefined) {
+        this.highlight = function (value) {
+          if (!value) return ""
+          const queryValue = this.getQueryValue().trim();
+          return queryValue
+            ? value.replace(new RegExp(queryValue, "gi"), "<strong>$&</strong>")
+            : value
+        };
+      }
+
+      if (this["autocomplete_control"] === undefined) {
+        this.autocomplete_control = function () {
           const autocompleteID = this.autocompleteID();
           return `
           <auto-complete src="${this.autocomplete_url()}" for="${autocompleteID}-popup" fetch-on-empty>
@@ -5312,71 +5317,77 @@ class Users {
             <ul id="${autocompleteID}-popup"></ul>
             <div id="${autocompleteID}-popup-feedback" class="visually-hidden"></div>
           </auto-complete>
-        ` };
+        `
+        };
       }
 
-      if (this['autocomplete_element_template'] === undefined) {
-        this.autocomplete_element_template = function(item) {
+      if (this["autocomplete_element_template"] === undefined) {
+        this.autocomplete_element_template = function (item) {
           return `<li role="option" data-autocomplete-value="${item.id}">${this.autocomplete_template(item)}</li>`
         };
       }
     },
 
-    queryTokenizer: function(query) {
-      return query.trim().toLowerCase().split(/\s+/).filter(Boolean);
+    queryTokenizer: function (query) {
+      return query.trim().toLowerCase().split(/\s+/).filter(Boolean)
     },
 
-    filterResults: function(data, query) {
+    filterResults: function (data, query) {
       const queryStrings = this.queryTokenizer(query);
       return data.filter(item => {
         const lowerTitle = item.title.toLowerCase();
-        return queryStrings.some(queryString => lowerTitle.includes(queryString));
-      });
+        return queryStrings.some(queryString =>
+          lowerTitle.includes(queryString)
+        )
+      })
     },
 
-    fetchAutocompleteResults: async function(url) {
+    fetchAutocompleteResults: async function (url) {
       const result = await fetchAutocompleteJSON(url);
       const transformed = this.transform_autocomplete_results(result);
       this.fetchedData = {};
-      transformed.map(item => this.fetchedData[item.id] = item);
-      return transformed.map(item => this.autocomplete_element_template(item)).join('');
+      transformed.map(item => (this.fetchedData[item.id] = item));
+      return transformed
+        .map(item => this.autocomplete_element_template(item))
+        .join("")
     },
 
-    fetchOnceAndFilterLocalResults: async function(url) {
+    fetchOnceAndFilterLocalResults: async function (url) {
       if (this.fetchedData === undefined) {
         await this.fetchAutocompleteResults(url);
       }
-      const query = url.searchParams.get('q');
+      const query = url.searchParams.get("q");
       const data = Object.values(this.fetchedData);
       const filteredData = query ? this.filterResults(data, query) : data;
-      return filteredData.map(item => this.autocomplete_element_template(item)).join('');
+      return filteredData
+        .map(item => this.autocomplete_element_template(item))
+        .join("")
     },
 
-    autocompleteID: function() {
-      return this.blockID + '-autocomplete';
+    autocompleteID: function () {
+      return this.blockID + "-autocomplete"
     },
 
-    getQueryValue: function() {
+    getQueryValue: function () {
       const completer = this.inner.querySelector("auto-complete > input");
-      return completer.value;
+      return completer.value
     },
 
-    addAutocompletetoSirTrevorForm: function() {
+    addAutocompletetoSirTrevorForm: function () {
       const completer = this.inner.querySelector("auto-complete");
       completer.fetchResult = this.autocomplete_fetch.bind(this);
-      completer.addEventListener('auto-complete-change', (e) => {
+      completer.addEventListener("auto-complete-change", e => {
         const data = this.fetchedData[e.relatedTarget.value];
         if (e.relatedTarget.value && data) {
-          e.value = e.relatedTarget.value = '';
+          e.value = e.relatedTarget.value = "";
           this.createItemPanel({ ...data, display: "true" });
         }
       });
-    },
-  },
-
+    }
+  };
 
   SirTrevor.Block.prototype.availableMixins.push("autocompleteable");
-})(jQuery);
+})();
 
 (function ($){
   SirTrevor.BlockMixins.Formable = {
