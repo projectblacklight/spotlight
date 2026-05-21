48ea790387af2a8f6ee66f15a807d21e6c939d0b
Steve Taylor <sctaylor@stanford.edu>
2026-05-20 20:20:21 -0700

    Update rubocop disable namespace

--- spec/models/spotlight/page_spec.rb
+++ spec/models/spotlight/page_spec.rb
@@ -84,7 +84,7 @@ describe Spotlight::Page, type: :model do
 
     it 'does not have content when the page is empty' do
       page.content = []
-      expect(page).not_to have_content # rubocop:disable Capybara/NegationMatcher
+      expect(page).not_to have_content # rubocop:disable Capybara/RSpec/NegationMatcher
     end
 
     it 'has content when the page has a widget' do
