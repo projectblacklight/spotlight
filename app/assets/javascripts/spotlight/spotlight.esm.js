f1a8b883b1fe91023467eef689a29a811fbbb57f
Justin Coyne <jcoyne@justincoyne.com>
2026-06-16 13:24:40 -0500

    Remove jquery dependency from spotlight/admin/croppable_modal.js

--- app/assets/javascripts/spotlight/spotlight.esm.js
+++ app/assets/javascripts/spotlight/spotlight.esm.js
@@ -4178,7 +4178,6 @@ class Crop {
 }
 
 class CroppableModal {
-
   attachModalHandlers() {
     // Attach handler for when modal first loads, to show the cropper
     this.attachModalLoadBehavior();
@@ -4188,58 +4187,116 @@ class CroppableModal {
 
   attachModalLoadBehavior() {
     // Listen for event thrown when modal is displayed with content
-    document.addEventListener('loaded.blacklight.blacklight-modal', function(e) {
-      var dataCropperDiv = $('#blacklight-modal [data-behavior="iiif-cropper"]');
-      
-      if(dataCropperDiv) {
-        new Crop(dataCropperDiv, false).render();
+    document.addEventListener(
+      "loaded.blacklight.blacklight-modal",
+      function (e) {
+        const dataCropperDiv = document.querySelector(
+          '#blacklight-modal [data-behavior="iiif-cropper"]'
+        );
+
+        if (dataCropperDiv) {
+          new Crop($(dataCropperDiv), false).render();
+        }
       }
-    });
+    );
   }
 
   // Field names are of the format item[item_0][iiif_image_id]
   iiifInputField(itemIndex, fieldName, parentElement) {
-    var itemPrefix = 'item[' + itemIndex + ']';
-    var selector = 'input[name="' + itemPrefix + '[' + fieldName + ']"]';
-    return $(selector, parentElement);
+    const itemPrefix = "item[" + itemIndex + "]";
+    const selector = 'input[name="' + itemPrefix + "[" + fieldName + ']"]';
+    return parentElement ? parentElement.querySelector(selector) : null
   }
 
   attachModalSaveHandler() {
-    var context = this;
-   
-    document.addEventListener('show.blacklight.blacklight-modal', function(e) {
-      $('#save-cropping-selection').on('click', () => {
-        context.saveCroppedRegion();
-      });
+    const context = this;
+
+    document.addEventListener("show.blacklight.blacklight-modal", function (e) {
+      const saveBtn = document.getElementById("save-cropping-selection");
+      if (saveBtn) {
+        saveBtn.addEventListener("click", () => {
+          context.saveCroppedRegion();
+        });
+      }
     });
   }
 
   saveCroppedRegion() {
     //On hitting "save changes", we need to copy over the value
     //to the iiif thumbnail url input field as well as the image source itself
-    var context = this;
-    var dataCropperDiv = $('#blacklight-modal [data-behavior="iiif-cropper"]');
+    const context = this;
+    const dataCropperDiv = document.querySelector(
+      '#blacklight-modal [data-behavior="iiif-cropper"]'
+    );
 
-    if(dataCropperDiv) {
-      var dataCropperKey = dataCropperDiv.data("cropper-key");
-      var itemIndex = dataCropperDiv.data("index-id");
+    if (dataCropperDiv) {
+      const dataCropperKey =
+        dataCropperDiv.dataset.cropperKey ||
+        dataCropperDiv.getAttribute("data-cropper-key");
+      const itemIndex =
+        dataCropperDiv.dataset.indexId ||
+        dataCropperDiv.getAttribute("data-index-id");
+
       // Get the element on the main edit page whose select image link opened up the modal
-      var itemElement = $('[data-cropper="' + dataCropperKey + '"]');
+      const itemElement = document.querySelector(
+        '[data-cropper="' + dataCropperKey + '"]'
+      );
+      if (!itemElement) return
+
       // Get the hidden input field on the main edit page corresponding to this item
-      var thumbnailSaveField = context.iiifInputField(itemIndex, 'thumbnail_image_url', itemElement);
-      var fullimageSaveField = context.iiifInputField(itemIndex, 'full_image_url', itemElement);
-      var iiifTilesource = context.iiifInputField(itemIndex, 'iiif_tilesource', itemElement).val();
-      var regionValue = context.iiifInputField(itemIndex, 'iiif_region', itemElement).val();
+      const thumbnailSaveField = context.iiifInputField(
+        itemIndex,
+        "thumbnail_image_url",
+        itemElement
+      );
+      const fullimageSaveField = context.iiifInputField(
+        itemIndex,
+        "full_image_url",
+        itemElement
+      );
+
+      const iiifTilesourceField = context.iiifInputField(
+        itemIndex,
+        "iiif_tilesource",
+        itemElement
+      );
+      const regionValueField = context.iiifInputField(
+        itemIndex,
+        "iiif_region",
+        itemElement
+      );
+
+      const iiifTilesource = iiifTilesourceField
+        ? iiifTilesourceField.value
+        : "";
+      const regionValue = regionValueField ? regionValueField.value : "";
+
       // Extract the region string to incorporate into the thumbnail URL
-      var urlPrefix = iiifTilesource.substring(0, iiifTilesource.lastIndexOf('/info.json'));
-      var thumbnailUrl = urlPrefix + '/' + regionValue + '/!400,400/0/default.jpg';
+      const lastIndex = iiifTilesource.lastIndexOf("/info.json");
+      const urlPrefix =
+        lastIndex !== -1
+          ? iiifTilesource.substring(0, lastIndex)
+          : iiifTilesource;
+      const thumbnailUrl =
+        urlPrefix + "/" + regionValue + "/!400,400/0/default.jpg";
+
       // Set the hidden input value to the thumbnail URL
       // Also set the full image - which is used by widgets like carousel or slideshow
-      thumbnailSaveField.val(thumbnailUrl);
-      fullimageSaveField.val(urlPrefix + '/' + regionValue + '/!800,800/0/default.jpg');
+      if (thumbnailSaveField) {
+        thumbnailSaveField.value = thumbnailUrl;
+        thumbnailSaveField.dispatchEvent(new Event("change", { bubbles: true }));
+      }
+      if (fullimageSaveField) {
+        fullimageSaveField.value =
+          urlPrefix + "/" + regionValue + "/!800,800/0/default.jpg";
+        fullimageSaveField.dispatchEvent(new Event("change", { bubbles: true }));
+      }
+
       // Also change img url for thumbnail image
-      var itemImage = $('img.img-thumbnail', itemElement);      
-      itemImage.attr('src', thumbnailUrl);
+      const itemImage = itemElement.querySelector("img.img-thumbnail");
+      if (itemImage) {
+        itemImage.setAttribute("src", thumbnailUrl);
+      }
     }
   }
 }
