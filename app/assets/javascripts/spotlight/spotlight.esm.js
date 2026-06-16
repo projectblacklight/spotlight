88ccf4ef4617e48011d8fd6a369a10d15816be7f
Justin Coyne <jcoyne@justincoyne.com>
2026-06-16 11:57:18 -0500

    Remove jquery dependency from spotlight/admin/blacklight_configuration.js

--- app/assets/javascripts/spotlight/spotlight.esm.js
+++ app/assets/javascripts/spotlight/spotlight.esm.js
@@ -3588,60 +3588,96 @@ class BlacklightConfiguration {
     this.addCheckboxToggleBehavior();
     this.addEnableToggleBehavior();
   }
-  
-  // Add Select/Deselect all behavior for metadata field names for a given view e.g. Item details. 
-  addCheckboxToggleBehavior() {
-    $("[data-behavior='metadata-select']").each(function(){
-      var selectCheckbox = $(this);
-      var parentCell = selectCheckbox.parents("th");
-      var table = parentCell.closest("table");
-      var columnRows = $("tr td:nth-child(" + (parentCell.index() + 1) + ")", table);
-      var checkboxes = $("input[type='checkbox']", columnRows);
-      updateSelectAllInput(selectCheckbox, columnRows);
-      // Add the check/uncheck behavior to the select/deselect all checkbox
-      selectCheckbox.on('click', function(e){
-        var allChecked = allCheckboxesChecked(columnRows);
-        columnRows.each(function(){
-          $("input[type='checkbox']", $(this)).prop('checked', !allChecked);
-        });
-      });
-      // When a single checkbox is selected/unselected, the "All" checkbox should be updated accordingly.
-      checkboxes.each(function(){
-        $(this).on('change', function(){
-          updateSelectAllInput(selectCheckbox, columnRows);
-        });
-      }); 
-    });
 
+  // Add Select/Deselect all behavior for metadata field names for a given view e.g. Item details.
+  addCheckboxToggleBehavior() {
     // Check number of checkboxes against the number of checked
     // checkboxes to determine if all of them are checked or not
-    function allCheckboxesChecked(elements) {
-      return ($("input[type='checkbox']", elements).length == $("input[type='checkbox']:checked", elements).length)
+    function allCheckboxesChecked(cells) {
+      let total = 0;
+      let checked = 0;
+      cells.forEach(cell => {
+        cell.querySelectorAll("input[type='checkbox']").forEach(cb => {
+          total++;
+          if (cb.checked) {
+            checked++;
+          }
+        });
+      });
+      return total === checked
     }
 
     // Check or uncheck the "All" checkbox for each view column, e.g. Item details, List, etc.
-    function updateSelectAllInput(checkbox, elements) {
-      if ( allCheckboxesChecked(elements) ) {
-        checkbox.prop('checked', true);
-      } else {
-        checkbox.prop('checked', false);
-      }
+    function updateSelectAllInput(checkbox, cells) {
+      checkbox.checked = allCheckboxesChecked(cells);
     }
-  }
-    
-  addEnableToggleBehavior() {
-    $("[data-behavior='enable-feature']").each(function(){
-      var checkbox = $(this);
-      var target = $($(this).data('target'));
 
-      checkbox.on('change', function() {
-        if ($(this).is(':checked')) {
-          target.find('input:checkbox').not("[data-behavior='enable-feature']").prop('checked', true).attr('disabled', false);
-        } else {
-          target.find('input:checkbox').not("[data-behavior='enable-feature']").prop('checked', false).attr('disabled', true);
-        }
+    document
+      .querySelectorAll("[data-behavior='metadata-select']")
+      .forEach(selectCheckbox => {
+        const parentCell = selectCheckbox.closest("th");
+        if (!parentCell) return
+
+        const table = parentCell.closest("table");
+        if (!table) return
+
+        const columnIndex = Array.from(parentCell.parentNode.children).indexOf(
+          parentCell
+        );
+        const columnRows = table.querySelectorAll(
+          `tr td:nth-child(${columnIndex + 1})`
+        );
+
+        const checkboxes = [];
+        columnRows.forEach(cell => {
+          cell.querySelectorAll("input[type='checkbox']").forEach(cb => {
+            checkboxes.push(cb);
+          });
+        });
+
+        updateSelectAllInput(selectCheckbox, columnRows);
+
+        // Add the check/uncheck behavior to the select/deselect all checkbox
+        selectCheckbox.addEventListener("click", () => {
+          const allChecked = allCheckboxesChecked(columnRows);
+          columnRows.forEach(cell => {
+            cell.querySelectorAll("input[type='checkbox']").forEach(cb => {
+              cb.checked = !allChecked;
+              cb.dispatchEvent(new Event("change", { bubbles: true }));
+            });
+          });
+          updateSelectAllInput(selectCheckbox, columnRows);
+        });
+
+        // When a single checkbox is selected/unselected, the "All" checkbox should be updated accordingly.
+        checkboxes.forEach(cb => {
+          cb.addEventListener("change", () => {
+            updateSelectAllInput(selectCheckbox, columnRows);
+          });
+        });
       });
-    });
+  }
+
+  addEnableToggleBehavior() {
+    document
+      .querySelectorAll("[data-behavior='enable-feature']")
+      .forEach(checkbox => {
+        const targetSelector = checkbox.dataset.target;
+        if (!targetSelector) return
+        const target = document.querySelector(targetSelector);
+        if (!target) return
+
+        checkbox.addEventListener("change", () => {
+          const isChecked = checkbox.checked;
+          target.querySelectorAll("input[type='checkbox']").forEach(cb => {
+            if (!cb.matches("[data-behavior='enable-feature']")) {
+              cb.checked = isChecked;
+              cb.disabled = !isChecked;
+              cb.dispatchEvent(new Event("change", { bubbles: true }));
+            }
+          });
+        });
+      });
   }
 }
 
