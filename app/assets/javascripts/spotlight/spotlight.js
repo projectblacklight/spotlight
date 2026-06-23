65e4a44c6610e42c49c22bf239949761bcc680b0
Justin Coyne <jcoyne@justincoyne.com>
2026-06-23 17:26:31 -0500

    Remove jquery from the remainder of the admin scripts

--- app/assets/javascripts/spotlight/spotlight.js
+++ app/assets/javascripts/spotlight/spotlight.js
@@ -3977,10 +3977,9 @@
     clickCallback,
     activeImageId,
   ) {
-    const element = panel && panel.jquery ? panel[0] : panel;
-    if (!element) return
+    if (!panel) return
 
-    initMultiImageSelector(element, image_versions, clickCallback, activeImageId);
+    initMultiImageSelector(panel, image_versions, clickCallback, activeImageId);
   }
 
   function addImageSelector(input, panel, manifestUrl, initialize) {
@@ -4013,7 +4012,7 @@
             function (selectorImage) {
               cropper.setIiifFields(selectorImage);
             },
-            cropper.iiifImageField.val(),
+            cropper.iiifImageField.value,
           );
         }
       });
@@ -4071,8 +4070,7 @@
 
   class Crop {
     constructor(cropArea, preserveAspectRatio = true) {
-      // Extract raw DOM element if cropArea is a jQuery object
-      this.cropArea = cropArea && cropArea.jquery ? cropArea[0] : cropArea;
+      this.cropArea = cropArea;
       if (this.cropArea) {
         this.cropArea.iiifCropper = this;
       }
@@ -4134,27 +4132,11 @@
         const selector = 'input[name="' + inputPrefix + "[" + fieldName + ']"]';
         const element = inputParentElement.querySelector(selector);
         if (element) {
-          if (!element.val) {
-            element.val = function (value) {
-              if (value === undefined) {
-                return this.value
-              } else {
-                this.value = value;
-                return this
-              }
-            };
-          }
           return element
         }
       }
       // Return a dummy object to prevent null-pointer exceptions
-      return {
-        value: undefined,
-        val: function (value) {
-          if (value === undefined) return undefined
-          return this
-        },
-      }
+      return { value: undefined }
     }
 
     // Render the cropper environment and add hooks into the autocomplete and upload forms
@@ -4167,12 +4149,12 @@
     // Setup the cropper on page load if the field
     // that holds the IIIF url is populated
     setupExistingIiifCropper() {
-      if (this.iiifUrlField.val() === "") {
+      if (this.iiifUrlField.value === "") {
         return
       }
 
       this.addImageSelectorToExistingCropTool();
-      this.setTileSource(this.iiifUrlField.val());
+      this.setTileSource(this.iiifUrlField.value);
     }
 
     // Display the IIIF Cropper map with the current IIIF Layer (and cropbox, once the layer is available)
@@ -4216,10 +4198,10 @@
 
     // Get (or initialize) the current crop region from the form data
     getCropRegion() {
-      var regionFieldValue = this.iiifRegionField.val();
+      var regionFieldValue = this.iiifRegionField.value;
       if (!regionFieldValue || regionFieldValue === "") {
         var region = this.defaultCropRegion();
-        this.iiifRegionField.val(region);
+        this.iiifRegionField.value = region;
         return region
       } else {
         return regionFieldValue.split(",")
@@ -4277,9 +4259,9 @@
     // the appropriate IIIF URL or identifier
     setIiifFields(iiifObject) {
       this.setTileSource(iiifObject.tilesource);
-      this.iiifManifestField.val(iiifObject.manifest);
-      this.iiifCanvasField.val(iiifObject.canvasId);
-      this.iiifImageField.val(iiifObject.imageId);
+      this.iiifManifestField.value = iiifObject.manifest;
+      this.iiifCanvasField.value = iiifObject.canvasId;
+      this.iiifImageField.value = iiifObject.imageId;
     }
 
     // Set the Crop tileSource and setup the cropper
@@ -4294,11 +4276,11 @@
       }
 
       if (this.cropBox) {
-        this.iiifRegionField.val("");
+        this.iiifRegionField.value = "";
       }
 
       this.tileSource = source;
-      this.iiifUrlField.val(source);
+      this.iiifUrlField.value = source;
       this.setupIiifCropper();
     }
 
@@ -4346,7 +4328,7 @@
           var bounds = e.layer.getBounds();
           var region = self.projectBoundsToIIIFRegion(bounds);
 
-          self.iiifRegionField.val(region.join(","));
+          self.iiifRegionField.value = region.join(",");
         },
       );
     }
@@ -4403,7 +4385,7 @@
     }
 
     addImageSelectorToExistingCropTool() {
-      if (this.iiifManifestField.val() === "") {
+      if (this.iiifManifestField.value === "") {
         return
       }
 
@@ -4425,8 +4407,8 @@
           addImageSelector(
             inputElement,
             panelElement,
-            this.iiifManifestField.val(),
-            !this.iiifImageField.val(),
+            this.iiifManifestField.value,
+            !this.iiifImageField.value,
           );
         }
       }
